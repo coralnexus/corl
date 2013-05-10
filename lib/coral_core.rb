005c17283bace477b1b664956bb295e008d843ef
Adrian Webb <adrian.webb@coraltech.net>
2013-05-10 03:02:47 -0400

    Undoing the last design decision regarding the splitting of coral_core into a basic and regular version.

--- lib/coral_core.rb
+++ lib/coral_core.rb
@@ -2,6 +2,8 @@
 home         = File.dirname(__FILE__)
 dependencies = File.join(home, 'dependency')
 
+git_location = Coral.location('git')
+
 $:.unshift(home) unless
   $:.include?(home) || $:.include?(File.expand_path(home))
   
@@ -36,26 +38,30 @@ end
 
 #---
 
-begin
-  require 'git'
+if git_location
+  begin
+    require 'git'
     
-rescue LoadError
-  git_lib = File.join(dependencies, 'git', 'lib')
+  rescue LoadError
+    git_lib = File.join(dependencies, 'git', 'lib')
   
-  $:.push(git_lib)
-  require File.join(git_lib, 'git.rb')  
+    $:.push(git_lib)
+    require File.join(git_lib, 'git.rb')  
+  end
 end
 
 #---
 
 # Include pre core utilities (no internal dependencies)
-[ :git, :data ].each do |name| 
-  require File.join('coral_core', 'util', name.to_s + ".rb") 
-end
+require File.join('coral_core', 'util', 'data.rb') 
 
-# Include Git overrides
-Dir.glob(File.join(home, 'coral_core', 'util', 'git', '*.rb')).each do |file|
-  require file
+if git_location
+  require File.join('coral_core', 'util', 'git.rb') 
+
+  # Include Git overrides
+  Dir.glob(File.join(home, 'coral_core', 'util', 'git', '*.rb')).each do |file|
+    require file
+  end
 end
 
 # Include core
@@ -72,8 +78,14 @@ end
 end
 
 # Include data model
-[ :event, :command, :repository, :memory ].each do |name| 
+[ :event, :command ].each do |name| 
   require File.join('coral_core', name.to_s + ".rb") 
+end
+
+if git_location
+  [ :repository, :memory ].each do |name| 
+    require File.join('coral_core', name.to_s + ".rb") 
+  end  
 end
 
 # Include specialized events
@@ -86,6 +98,132 @@ Dir.glob(File.join(home, 'coral_core', 'template', '*.rb')).each do |file|
   require file
 end
 
-#---
+#*******************************************************************************
+# Coral Core Library
+#
+# This provides core data elements and utilities used in the Coral gems.
+#
+# Author::    Adrian Webb (mailto:adrian.webb@coraltech.net)
+# License::   GPLv3
+module Coral
+  
+  VERSION = File.read(File.join(File.dirname(__FILE__), '..', 'VERSION'))
+  
+  #---
+  
+  @@ui = Coral::Core.ui
+  
+  #---
+  
+  def self.ui
+    return @@ui
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Initialization
+  
+  def self.load(base_path)
+    if File.exists?(base_path)
+      Dir.glob(File.join(base_path, '*.rb')).each do |file|
+        require file
+      end
+      Dir.glob(File.join(base_path, 'event', '*.rb')).each do |file|
+        require file
+      end
+      Dir.glob(File.join(base_path, 'template', '*.rb')).each do |file|
+        require file
+      end  
+    end  
+  end
+  
+  #---
+  
+  @@initialized = false
+  
+  def self.initialize
+    unless @@initialized
+      Config.set_property('time', Time.now.to_i)
+      
+      # Include Coral extensions
+      Puppet::Node::Environment.new.modules.each do |mod|
+        load(File.join(mod.path, 'lib', 'coral'))
+      end      
+            
+      @@initialized = true
+    end    
+  end
+  
+  #-----------------------------------------------------------------------------
+  # External execution
+   
+  def self.run
+    begin
+      initialize
+      yield
+      
+    rescue Exception => error
+      ui.warning(error.inspect)
+      ui.warning(Util::Data.to_yaml(error.backtrace))
+      raise
+    end
+  end
+  
+  #---  
+    
+  def self.location(command)
+    exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+      exts.each do |ext|
+        exe = File.join(path, "#{command}#{ext}")
+        return exe if File.executable?(exe)
+      end
+    end
+    return nil
+  end 
+end
 
-require 'coral.rb'
+#-------------------------------------------------------------------------------
+# Global namespace
+
+module Kernel  
+  def dbg(data, label = '')
+    require 'pp'
+    
+    puts '>>----------------------'
+    unless label.empty?
+      puts label
+      puts '---'
+    end
+    pp data
+    puts '<<'
+  end
+end
+
+#-------------------------------------------------------------------------------
+# Data type alterations
+
+class Hash
+  def search(search_key, options = {})
+    config = Coral::Config.ensure(options)
+    value  = nil
+    
+    recurse       = config.get(:recurse, false)
+    recurse_level = config.get(:recurse_level, -1)
+        
+    self.each do |key, data|
+      if key == search_key
+        value = data
+        
+      elsif data.is_a?(Hash) && 
+        recurse && (recurse_level == -1 || recurse_level > 0)
+        
+        recurse_level -= 1 unless recurse_level == -1
+        value = value.search(search_key, 
+          Coral::Config.new(config).set(:recurse_level, recurse_level)
+        )
+      end
+      break unless value.nil?
+    end
+    return value
+  end
+end
