714869ecda3d519d978d6839cd7f0616e2a6720f
Adrian Webb <adrian.webb@coraltech.net>
2013-06-08 17:28:28 -0400

    Adding a filename method to the disk utility class (so file names can be arrays).

--- lib/coral_core/util/disk.rb
+++ lib/coral_core/util/disk.rb
@@ -14,6 +14,12 @@ class Disk
   #-----------------------------------------------------------------------------
   # Utilities
   
+  def self.filename(file_name)
+    return ( file_name.is_a?(Array) ? file_name.join(File::SEPARATOR) : file_name.to_s )
+  end
+  
+  #---
+  
   def self.open(file_name, options = {}, reset = false)
     mode          = options[:mode].to_s
     
