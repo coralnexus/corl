9ed5bfa87f59867d1d85fae51ff08b4a65965a2e
Adrian Webb <adrian.webb@coraltech.net>
2013-06-25 05:42:39 -0400

    Updating the name puppet function to utilize the new provisioner interface.

--- lib/puppet/parser/functions/name.rb
+++ lib/puppet/parser/functions/name.rb
@@ -14,7 +14,7 @@ This function returns a standardized form of a given resource name.
       raise(Puppet::ParseError, "name(): Must have a resource name specified; " +
         "given (#{args.size} for 1)") if args.size < 1
       
-      name = Coral::Resource.to_name(args[0], :puppet)
+      name = Coral.provisioner(:puppet).to_name(args[0])
     end
     return name
   end
