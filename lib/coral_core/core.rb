0ac71af29a1a2967fa4338453f5a262c63251c6e
Adrian Webb <adrian.webb@coraltech.net>
2013-07-02 16:37:54 -0400

    Updating the core to extend the config class.

--- lib/coral_core/core.rb
+++ lib/coral_core/core.rb
@@ -1,6 +1,6 @@
 
 module Coral
-class Core
+class Core < Config
   
   #-----------------------------------------------------------------------------
   # Properties
@@ -53,99 +53,6 @@ class Core
  
   #-----------------------------------------------------------------------------
   # General utilities
-
-  def self.symbol_map(data)
-    return Util::Data.symbol_map(data)
-  end
-  
-  #---
-  
-  def symbol_map(data)
-    return self.class.symbol_map(data)
-  end
-  
-  #---
-  
-  def self.string_map(data)
-    return Util::Data.string_map(data)
-  end
-  
-  #---
-  
-  def string_map(data)
-    return self.class.string_map(data)
-  end
-  
-  #-----------------------------------------------------------------------------
-      
-  def self.filter(data, method = false)
-    return Util::Data.filter(data, method)
-  end
-  
-  #---
-  
-  def filter(data, method = false)
-    return self.class.filter(data, method)
-  end
-    
-  #-----------------------------------------------------------------------------
-          
-  def self.array(data, default = [], split_string = false)
-    return Util::Data.array(data, default, split_string)
-  end
-  
-  #---
-  
-  def array(data, default = [], split_string = false)
-    return self.class.array(data, default, split_string)
-  end
-    
-  #---
-        
-  def self.hash(data, default = {})
-    return Util::Data.hash(data, default)
-  end
-  
-  #---
-  
-  def hash(data, default = {})
-    return self.class.hash(data, default)
-  end
-    
-  #---
-         
-  def self.string(data, default = '')
-    return Util::Data.string(data, default)
-  end
-  
-  #---
-  
-  def string(data, default = '')
-    return self.class.string(data, default)
-  end
-    
-  #---
-         
-  def self.symbol(data, default = :undefined)
-    return Util::Data.symbol(data, default)
-  end
-  
-  #---
-  
-  def symbol(data, default = '')
-    return self.class.symbol(data, default)
-  end
-     
-  #---
-    
-  def self.test(data)
-    return Util::Data.test(data)
-  end
-  
-  #---
-  
-  def test(data)
-    return self.class.test(data)
-  end  
+ 
 end
 end
