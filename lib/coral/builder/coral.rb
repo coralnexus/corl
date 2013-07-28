f8e3d2970d441ab2dbedd184f5059ae820bb4967
Adrian Webb <adrian.webb@coraltech.net>
2013-07-27 20:17:29 -0400

    RUBYCORE-72 - Adding a basic coral builder plugin that just encapsulates the base builder plugin (no extra functionality).

---
+++ lib/coral/builder/coral.rb
@@ -0,0 +1,11 @@
+
+module Coral
+module Builder
+class Coral < Plugin::Builder
+   
+  #-----------------------------------------------------------------------------
+  # Builder operations
+   
+end
+end
+end
