326c26781b5052a42726aa97ab8f95560a88e6b0
Adrian Webb <adrian.webb@coraltech.net>
2013-08-26 13:17:41 -0400

    RUBYCORE-316 - Splitting the plugin interface macro from the object interface macro.

--- lib/coral_core/mixin/macro/object_interface.rb
+++ lib/coral_core/mixin/macro/object_interface.rb
@@ -1,11 +1,12 @@
 
 # Should be included via extend
 #
-# extend Mixin::ObjectInterface
+# extend Mixin::Macro::ObjectInterface
 #
 
 module Coral
 module Mixin
+module Macro
 module ObjectInterface
   
   include Mixin::SubConfig
@@ -15,279 +16,262 @@ module ObjectInterface
   # Object collections
   
   @@object_types = {}
-  
+
   #---
   
-  def object_collection(type, plural, ensure_proc, delete_proc = nil, search_proc = nil)
-    @@object_types[type] = {
-      :plural      => plural,
-      :ensure_proc => ensure_proc,
-      :delete_proc => delete_proc,
-      :search_proc => search_proc
-    }
+  def object_collection(_type, _ensure_proc, _method_options = {})
+    _method_config = Config.ensure(_method_options).import(:ensure_proc => _ensure_proc)
     
-    #---
+    _plural      = _method_config.init(:plural, "#{_type}s").get(:plural)
+    _delete_proc = _method_config.get(:delete_proc)
+    _search_proc = _method_config.get(:search_proc)
+    
+    @@object_types[_type] = _method_config
+    
+    #---------------------------------------------------------------------------
     
     object_utilities
     
     #---
     
-    define_method "#{type}_info" do |name = nil|
-      symbol_map( name ? get([ plural, name ], {}) : get(plural, {}) )
+    unless respond_to? :each_object!
+      define_method :each_object! do |object_types = nil|
+        each_object_type!(object_types) do |type, plural, options|
+          send(plural).each do |name, obj|
+            yield(type, name, obj)  
+          end 
+        end  
+      end
+    end
+     
+    #---------------------------------------------------------------------------
+    
+    define_method "#{_type}_config" do |name = nil|
+      Config.new( name ? get([ _plural, name ], {}) : get(_plural, {}) )
     end
   
     #---
     
-    define_method "#{type}_setting" do |name, property, default = nil, format = false|
-      get([ plural, name, property ], default, format)
+    define_method "#{_type}_setting" do |name, property, default = nil, format = false|
+      get([ _plural, name, property ], default, format)
     end
   
     #---
     
-    define_method "search_#{type}" do |name, keys, default = '', format = false|
-      search(type, name, keys, default, format)
+    define_method "search_#{_type}" do |name, keys, default = '', format = false|
+      search(_type, name, keys, default, format)
     end
     
     #---
     
-    define_method "#{plural}" do
-      _get(plural, {})
+    define_method "#{_plural}" do
+      _get(_plural, {})
     end
     
     #---
   
-    define_method "init_#{plural}" do
-      data = search_proc.call if search_proc
-      data = get_hash(plural) unless data
+    define_method "init_#{_plural}" do
+      data = hash(_search_proc.call) if _search_proc
+      data = get_hash(_plural) unless data
       
-      symbol_map(data).each do |name, info|
+      symbol_map(data).each do |name, options|
         if name != :settings
-          info[:object_container] = self
+          options[:object_container] = self
           
-          obj = ensure_proc.call(name, info)
-          _set([ plural, name ], obj)
+          obj = _ensure_proc.call(name, options)
+          _set([ _plural, name ], obj)
         end
       end
     end
   
     #---
 
-    define_method "set_#{plural}" do |data = {}|
-      data = symbol_map(hash(data))
+    define_method "set_#{_plural}" do |data = {}|
+      data = Config.ensure(data).export
     
-      send("clear_#{plural}")
-      set(plural, data)
+      send("clear_#{_plural}")
+      set(_plural, data)
     
-      data.each do |name, info|
-        info[:object_container] = self
+      data.each do |name, options|
+        options[:object_container] = self
         
-        obj = ensure_proc.call(name, info)
-        _set([ plural, name ], obj)
+        obj = _ensure_proc.call(name, options)
+        _set([ _plural, name ], obj)
       end
       self
     end
 
     #---
     
-    define_method "#{type}" do |name|
-      _get([ plural, name ])
+    define_method "#{_type}" do |name|
+      _get([ _plural, name ])
     end
     
     #---
 
-    define_method "set_#{type}" do |name, info = {}|
-      info = symbol_map(hash(info))
+    define_method "set_#{_type}" do |name, options = {}|
+      options = Config.ensure(options).export
       
-      set([ plural, name ], info)
+      set([ _plural, name ], options)
     
       info[:object_container] = self
       
-      obj = ensure_proc.call(name, info) 
-      _set([ plural, name ], obj)
+      obj = _ensure_proc.call(name, options) 
+      _set([ _plural, name ], obj)
       self
     end
   
     #---
   
-    define_method "set_#{type}_setting" do |name, property, value = nil|
-      set([ plural, name, property ], value)
+    define_method "set_#{_type}_setting" do |name, property, value = nil|
+      set([ _plural, name, property ], value)
       self
     end
     
     #---
 
-    define_method "delete_#{type}" do |name|
-      obj = send(type, name)
+    define_method "delete_#{_type}" do |name|
+      obj = send(_type, name)
     
-      delete([ plural, name ])
-      _delete([ plural, name ])
+      delete([ _plural, name ])
+      _delete([ _plural, name ])
     
-      delete_proc.call(obj) if delete_proc
+      _delete_proc.call(obj) if _delete_proc
       self
     end
   
     #---
   
-    define_method "delete_#{type}_setting" do |name, property|
-      delete([ plural, name, property ])
+    define_method "delete_#{_type}_setting" do |name, property|
+      delete([ _plural, name, property ])
       self
     end
   
     #---
   
-    define_method "clear_#{plural}" do
-      _get(plural).keys.each do |name|
-        send("delete_#{type}", name)
+    define_method "clear_#{_plural}" do
+      _get(_plural).keys.each do |name|
+        send("delete_#{_type}", name)
       end
       self
-    end     
+    end    
+        
+    #---------------------------------------------------------------------------
+  
+    define_method "search_#{_type}" do |name, keys, default = '', format = false|
+      obj_config = send("#{_type}_config", name)
+      search_object(obj_config, keys, default, format)
+    end
   end
-  
-  #---
-  
-  def plugin_collection(type, plural = nil, search_proc = nil)
-    plural = "#{type}s" unless plural
-    
-    object_collection(type, plural, 
-      Proc.new { |provider, options| Coral.plugin(type, provider, options) },
-      Proc.new { |plugin| Coral.remove_plugin(plugin) },
-      search_proc
-    )
-    @@object_types[type][:plugin] = true    
-  end
-  
+
   #-----------------------------------------------------------------------------
   # Utilities
   
-  @@utilities_initialized = false
-  
-  #---
-
   def object_utilities
     
-    return if @@utilities_initialized
-    @@utilities_initialized = true
-    
-    #---
-    
-    define_method :foreach_object_type! do |object_types = nil, plugins_only = false|
-      object_types = @@object_types.keys unless object_types
-      object_types = [ object_types ] unless object_types.is_a?(Array)
+    unless respond_to? :foreach_object_type!
+      define_method :foreach_object_type! do |object_types = nil, filter_proc = nil|
+        object_types = @@object_types.keys unless object_types
+        object_types = [ object_types ] unless object_types.is_a?(Array)
       
-      object_types.keys.each do |type|
-        unless plugins_only && ! @@object_types[type][:plugin]
-          plural = @@object_types[type][:plural]
-          yield(type, plural, @@object_types[type])
-        end
-      end  
-    end
-    
-    #---
-    
-    define_method :foreach_object! do |object_types = nil|
-      foreach_object_type!(object_types, false) do |type, plural, options|
-        send(plural).each do |name, obj|
-          yield(type, name, obj)  
-        end 
-      end  
-    end
-    
-    #---
-    
-    define_method :foreach_plugin! do |plugin_types = nil|
-      foreach_object_type!(plugin_types, true) do |type, plural, options|
-        send(plural).each do |name, plugin|
-          yield(type, name, plugin)  
-        end 
-      end  
-    end
-       
-    #---
-    
-    define_method :init_objects do |object_types = nil|
-      foreach_object_type!(object_types) do |type, plural, options|
-        send("init_#{plural}")  
-      end   
-    end
-    
-    #---
-    
-    define_method :clear_objects do |object_types = nil|
-      foreach_object_type!(object_types) do |type, plural, options|
-        send("clear_#{plural}")  
+        object_types.keys.each do |type|
+          unless filter_proc && ! filter_proc.call(type, @@object_types[type])
+            plural = @@object_types[type][:plural]
+            yield(type, plural, @@object_types[type])
+          end
+        end  
       end
     end
-
+           
+    #---
+    
+    unless respond_to? :init_objects
+      define_method :init_objects do |object_types = nil, filter_proc = nil|
+        foreach_object_type!(object_types, filter_proc) do |type, plural, options|
+          send("init_#{plural}")  
+        end   
+      end
+    end
+    
+    #---
+    
+    unless respond_to? :clear_objects
+      define_method :clear_objects do |object_types = nil, filter_proc = nil|
+        foreach_object_type!(object_types, filter_proc) do |type, plural, options|
+          send("clear_#{plural}")  
+        end
+      end
+    end
+    
     #---------------------------------------------------------------------------
   
-    #
-    # search(:provisioner, :puppet, [:resources, :defaults, :coral_base_git])
-    # search(:provisioner, :puppet, [:resources, :profiles, :coral_base])
-    # search(:provisioner, :puppet, [:nodes, :coral_base])
-    # search(:provisioner, :puppet, [:modules, :stdlib])
-    # search(:config, :json, [:identity, :url])
-    #   
-    define_method :search do |type, name, keys, default = '', format = false|
-      type_config = Config.new(send("#{type}_info", name))
-      value       = type_config.get(keys)
+    unless respond_to? :search_object  
+      define_method :search_object do |obj_config, keys, default = '', format = false|
+        value = obj_config.get(keys)
        
-      return filter(value, format) if value && ! value.is_a?(Hash)
+        unless ! value || value.is_a?(Hash)
+          settings = {}
     
-      settings = {}
-    
-      keys     = [ keys ] unless keys.is_a?(Array)
-      temp     = keys.dup
+          keys     = [ keys ] unless keys.is_a?(Array)
+          temp     = keys.dup
       
-      until temp.empty? do
-        if type_settings = type_config.delete([ temp, :settings ])
-          array(type_settings).each do |group_name|
-            if group_settings = settings(group_name)
-              settings = Util::Data.merge([ group_settings, settings ], true)  
+          until temp.empty? do
+            if obj_settings = obj_config.delete([ temp, :settings ])
+              array(obj_settings).each do |group_name|
+                if group_settings = settings(group_name)
+                  settings = Util::Data.merge([ group_settings, settings ], true)  
+                end
+              end            
             end
-          end            
-        end
-        temp.pop
-      end
-    
-      if type_settings = type_config.delete(:settings)
-        array(type_settings).each do |group_name|
-          if group_settings = settings(group_name)
-            settings = Util::Data.merge([ group_settings, settings ], true)  
+            temp.pop
           end
-        end            
-      end    
-        
-      unless settings.empty?
-        if value
-          value = Util::Data.merge([ settings, value ], true)         
-        else
-          value = settings[keys.last] if settings.has_key?(keys.last)
-        end
-      end
     
-      value = default if Util::Data.undef?(value)
-      return filter(value, format)
+          if obj_settings = obj_config.delete(:settings)
+            array(obj_settings).each do |group_name|
+              if group_settings = settings(group_name)
+                settings = Util::Data.merge([ group_settings, settings ], true)  
+              end
+            end            
+          end    
+        
+          unless settings.empty?
+            if value
+              value = Util::Data.merge([ settings, value ], true)         
+            else
+              value = settings[keys.last] if settings.has_key?(keys.last)
+            end
+          end
+    
+          value = default if Util::Data.undef?(value)
+        end
+        filter(value, format)
+      end
     end
     
     #---------------------------------------------------------------------------
     # Configuration loading saving
     
-    define_method :load do |options = {}|
-      if config.respond_to?(:load)
-        clear_objects  
-        config.load(options)    
-        init_objects
+    unless respond_to? :load
+      define_method :load do |options = {}|
+        if config.respond_to?(:load)
+          clear_objects  
+          config.load(options)    
+          init_objects
+        end
+        self  
       end
-      return self  
     end
     
     #---
     
-    define_method :save do |options = {}|
-      config.save(options) if config.respond_to?(:save)
-      return self  
+    unless respond_to? :save
+      define_method :save do |options = {}|
+        config.save(options) if config.respond_to?(:save)
+        self  
+      end
     end    
   end
+end
 end
 end
 end
---
+++ lib/coral_core/mixin/macro/plugin_interface.rb
@@ -0,0 +1,286 @@
+
+# Should be included via extend
+#
+# extend Mixin::Macro::PluginInterface
+#
+
+coral_require(File.dirname(__FILE__), :object_interface)
+
+#---
+
+module Coral
+module Mixin
+module Macro
+module PluginInterface
+  
+  include Mixin::Macro::ObjectInterface
+  
+  #-----------------------------------------------------------------------------
+  # Plugin collections
+  
+  def plugin_collection(_type, _method_options = {})
+    _method_config = Config.ensure(_method_options)
+    _method_config.set(:plugin, true)
+    
+    _plural          = _method_config.init(:plural, "#{_type}s").get(:plural)
+    _search_proc     = _method_config.get(:search_proc)
+    _single_instance = _method_config.get(:single_instance, false)
+    _plugin_type     = _method_config.get(:plugin_type, _type)
+    
+    @@object_types[_type] = _method_config
+    
+    #---------------------------------------------------------------------------
+    
+    object_utilities
+      
+    #---
+    
+    unless respond_to? :each_plugin!
+      define_method :each_plugin! do |plugin_types = nil, providers = nil|
+        providers = [ providers ] if providers && ! providers.is_a?(Array)
+        
+        filter_proc = Proc.new {|type, config| config[:plugin] }
+        each_object_type!(plugin_types, filter_proc) do |type, plural, options|
+          send(plural).each do |provider, plugins|
+            unless providers && ! providers.include?(provider)
+              if plugins.is_a?(Hash)
+                plugins.each do |name, plugin|
+                  yield(type, provider, plugin)  
+                end
+              else
+                yield(type, provider, plugin)
+              end
+            end
+          end 
+        end  
+      end
+    end
+    
+    #---
+    
+    define_method "each_#{_type}!" do |providers = nil|
+      each_plugin!(_type, providers) do |type, provider, plugin|
+        yield(type, provider, plugin)    
+      end
+    end
+    
+    #---------------------------------------------------------------------------
+    
+    if _single_instance
+      define_method "#{_type}_config" do |provider|
+        Config.new(get([ _type, provider ], {}))
+      end
+      
+      #---
+      
+      define_method "#{_type}_setting" do |provider, property, default = nil, format = false|
+        get([ _type, provider, property ], default, format)
+      end
+      
+      #---
+      
+      define_method "search_#{_type}" do |provider, keys, default = '', format = false|
+        search(_type, nil, keys, default, format)
+      end
+      
+    #---------------------------------------------------------------------------  
+    else
+      define_method "#{_type}_config" do |provider, name = nil|
+        Config.new( name ? get([ _plural, provider, name ], {}) : get(_plural, provider, {}) )
+      end
+      
+      #---
+      
+      define_method "#{_type}_setting" do |provider, name, property, default = nil, format = false|
+        get([ _plural, provider, name, property ], default, format)
+      end
+      
+      #---
+      
+      define_method "search_#{_type}" do |provider, name, keys, default = '', format = false|
+        search(_type, name, keys, default, format)
+      end
+    end
+   
+    #---------------------------------------------------------------------------
+    
+    define_method "#{_plural}" do |provider = nil|
+      ( provider ? _get([ _plural, provider ], {}) : _get(_plural, {}) )
+    end
+    
+    #---
+  
+    define_method "init_#{_plural}" do |providers = nil|
+      data = hash(_search_proc.call) if _search_proc
+      data = get_hash(_plural) unless data
+      
+      providers = [ providers ] if providers && ! providers.is_a?(Array)
+      
+      symbol_map(data).each do |provider, instance_settings|
+        if ! providers || providers.include?(provider)
+          if _single_instance
+            plugin = Coral.plugin(_plugin_type, provider, instance_settings)              
+            plugin.plugin_parent = self
+          
+            _set([ _plural, provider ], plugin)
+          else
+            instance_settings.each do |name, options|
+              if name != :settings
+                plugin = Coral.plugin(_plugin_type, provider, options)              
+                plugin.plugin_parent = self
+          
+                _set([ _plural, provider, name ], plugin)
+              end
+            end
+          end
+        end
+      end
+    end
+  
+    #---
+
+    define_method "set_#{_plural}" do |data = {}|
+      data = Config.ensure(data).export
+    
+      send("clear_#{_plural}")
+      set(_plural, data)
+    
+      data.each do |provider, instance_settings|
+        if _single_instance
+          plugin = Coral.plugin(_plugin_type, provider, instance_settings)              
+          plugin.plugin_parent = self
+          
+          _set([ _plural, provider ], plugin)  
+        else
+          instance_settings.each do |name, options|
+            plugin = Coral.plugin(_plugin_type, provider, options)
+            plugin.plugin_parent = self
+        
+            _set([ _plural, provider, name ], plugin)  
+          end
+        end
+      end
+      self
+    end
+    
+    #---
+    
+    define_method "clear_#{_plural}" do
+      _get(_plural).keys.each do |name|
+        send("delete_#{_type}", name)
+      end
+      self
+    end
+
+    #---------------------------------------------------------------------------
+    
+    if _single_instance
+      define_method "#{_type}" do |provider|
+        _get([ _plural, provider ])
+      end
+      
+      #---
+      
+      define_method "set_#{_type}" do |provider, options = {}|
+        options = Config.ensure(options).export
+      
+        set([ _plural, provider ], options)
+    
+        plugin = Coral.plugin(_plugin_type, provider, options)
+        plugin.plugin_parent = self
+        
+        _set([ _plural, provider ], plugin)
+        self
+      end
+          
+      #---
+
+      define_method "set_#{_type}_setting" do |provider, property, value = nil|
+        set([ _plural, provider, property ], value)
+        self
+      end
+    
+      #---
+
+      define_method "delete_#{_type}" do |provider|
+        plugin = send(_type, provider)
+    
+        delete([ _plural, provider ])
+        _delete([ _plural, provider ])
+    
+        Coral.remove_plugin(plugin)
+        self
+      end
+  
+      #---
+  
+      define_method "delete_#{_type}_setting" do |provider, property|
+        delete([ _plural, provider, property ])
+        self
+      end
+      
+      #---
+      
+      define_method "search_#{_type}" do |provider, keys, default = '', format = false|
+        plugin_config = send("#{_type}_config", provider)
+        search_object(plugin_config, keys, default, format)      
+      end
+      
+    #---------------------------------------------------------------------------
+    else
+      define_method "#{_type}" do |provider, name|
+        _get([ _plural, provider, name ])
+      end
+      
+      #---
+      
+      define_method "set_#{_type}" do |provider, name, options = {}|
+        options = Config.ensure(options).export
+      
+        set([ _plural, provider, name ], options)
+    
+        plugin = Coral.plugin(_plugin_type, provider, options)
+        plugin.plugin_parent = self
+        
+        _set([ _plural, provider, name ], plugin)
+        self
+      end
+          
+      #---
+
+      define_method "set_#{_type}_setting" do |provider, name, property, value = nil|
+        set([ _plural, provider, name, property ], value)
+        self
+      end
+    
+      #---
+
+      define_method "delete_#{_type}" do |provider, name|
+        plugin = send(_type, provider, name)
+    
+        delete([ _plural, provider, name ])
+        _delete([ _plural, provider, name ])
+    
+        Coral.remove_plugin(plugin)
+        self
+      end
+  
+      #---
+  
+      define_method "delete_#{_type}_setting" do |provider, name, property|
+        delete([ _plural, provider, name, property ])
+        self
+      end
+      
+      #---
+      
+      define_method "search_#{_type}" do |provider, name, keys, default = '', format = false|
+        plugin_config = send("#{_type}_config", provider, name)
+        search_object(plugin_config, keys, default, format)      
+      end
+    end  
+  end
+end
+end
+end
+end
