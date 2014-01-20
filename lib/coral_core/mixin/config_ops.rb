afd60e560617d4ac80a90d8a56daa8606608ec37
Adrian Webb <adrian.webb@coraltech.net>
2014-01-20 03:46:37 -0500

    Fixing wrong property name in the config operations mixin.

--- lib/coral_core/mixin/config_ops.rb
+++ lib/coral_core/mixin/config_ops.rb
@@ -13,10 +13,11 @@ module ConfigOps
   def parse(statement, options = {})
     config = Config.ensure(options)
     
-    # statement = 'common->php::apache::memory_limit = 32M'
-    # statement = 'identity -> test -> users::user[admin][shell]'
-    # statement = 'servers->development->dev.loc->facts[server_environment]=vagrant'
-    # statement = 'cloud->settings[debug][puppet][options] = ["--debug"]'
+    # statement =  common->php::apache::memory_limit = 32M
+    # statement =  identity -> test -> users::user[admin][shell]
+    # statement =  nodes->development->dev.loc->facts[server_location]
+    # statement =  coral->nodes[vagrant][dev.loc][vm][private_network]=172.5.5.10
+    # statement =  coral->settings[debug][puppet][options] = ["--debug"]
     
     reference, new_value = statement.split(/\=/)
     new_value            = new_value.join('=').strip if new_value && new_value.is_a?(Array)
@@ -26,9 +27,9 @@ module ConfigOps
     config_file     = config_elements.pop
     
     if config_directory = config.get(:directory, nil)
-      config_path = File.join(repo.directory, config_directory, *config_elements)
+      config_path = File.join(project.directory, config_directory, *config_elements)
     else
-      config_path = File.join(repo.directory, *config_elements)
+      config_path = File.join(project.directory, *config_elements)
     end
     
     return nil unless property && config_file
