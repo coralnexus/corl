471cb7a638bd3d9bd96c3cc3eb9d179e2b9f1663
Adrian Webb <adrian.webb@coraltech.net>
2014-01-30 05:13:47 -0500

    Further implementation of the node cli mixin methods.

--- lib/coral_core/mixin/cli/node.rb
+++ lib/coral_core/mixin/cli/node.rb
@@ -47,11 +47,9 @@ module Node
     if network.has_nodes? && ! options[:nodes].empty?
       # Execute action on remote nodes      
       nodes   = translate_node_references(options[:nodes], network)
-      success = true
-      
       success = Coral.batch(options[:parallel]) do |batch|      
         nodes.each do |node|
-          batch.add(node.name) do 
+          batch.add(node.name) do
             node.action(plugin_provider, params)
           end
         end
@@ -67,37 +65,93 @@ module Node
   # Utilities
   
   def translate_node_references(references, network)
-    node_map = {}
+    info  = node_info(references, network)    
+    nodes = []
     
-    references.each do |reference|
-      info = Plugin::Node.translate_reference(reference)
-      
-      unless info
-        info = { :provider => options[:node_provider], :name => reference }
-      end
-      
-      provider = info[:provider].to_sym
-      
-      node_map[provider] = [] unless node_map.has_key?(provider)
-      node_map[provider] << info[:name]
-    end
+    registered_nodes = network.nodes
     
-    node_groups = []
-    network.nodes.each do |provider, nodes|
-      nodes.each do |node_name, node|
-        node_groups << node.search(:groups)
+    info.each do |provider, names|
+      provider_nodes = registered_nodes[provider.to_sym]
+      
+      names.each do |name|
+        nodes << provider_nodes[name.to_sym]  
       end
     end
     
-    return node_map.values  
+    nodes  
   end
   protected :translate_node_references
   
   #---
   
-  def local_node(network)
+  def node_info(references, network)
+    groups    = symbol_map(node_groups(network))
+    node_info = {}
     
+    references.each do |reference|
+      info = Plugin::Node.translate_reference(reference)
+      info = { :provider => options[:node_provider], :name => reference } unless info
+      name = info[:name].to_sym     
+      
+      # Check for group membership
+      if groups.has_key?(name)
+        groups[name].each do |member_info|
+          provider = member_info[:provider].to_sym
+          
+          node_info[provider] = [] unless node_info.has_key?(provider)        
+          node_info[provider] << member_info[:name].to_s  
+        end
+      else
+        # Not a group
+        provider = info[:provider].to_sym
+        
+        if network.nodes.has_key?(provider)
+          node_found = false
+          
+          network.nodes(provider).each do |node_name, node|
+            if node_name == name
+              node_info[provider] = [] unless node_info.has_key?(provider)        
+              node_info[provider] << node_name
+              node_found = true
+              break
+            end
+          end
+          
+          unless node_found
+            # TODO:  Error or something?
+          end
+        end 
+      end     
+    end
+    
+    node_info  
   end
+  protected :node_info
+  
+  #---
+  
+  def node_groups(network)
+    groups = {}
+    
+    network.nodes.each do |provider, nodes|
+      nodes.each do |node_name, node|
+        node.search(:groups, [], :array).each do |group|
+          groups[group] = [] unless groups.has_key?(group)
+          groups[group] << { :provider => provider, :name => node_name }
+        end
+      end
+    end
+    
+    groups
+  end
+  protected :node_groups
+  
+  #---
+  
+  def local_node(network)
+    nil  # Coming soon 
+  end
+  protected :local_node
 end
 end
 end
