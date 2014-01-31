6a5a85995d9c06b357c70bb9802c8b6416566800
Adrian Webb <adrian.webb@coraltech.net>
2014-01-31 04:27:48 -0500

    Adding a groups method and some mostly cosmetic refactoring in the base node plugin.

--- lib/coral_core/plugin/node.rb
+++ lib/coral_core/plugin/node.rb
@@ -21,8 +21,6 @@ class Node < Base
     return plugin_parent
   end
  
-  #---
-  
   def network=network
     self.plugin_parent = network
   end
@@ -33,20 +31,14 @@ class Node < Base
     return network.node_setting(plugin_provider, name, property, default, format)
   end
   
-  #---
-  
   def search(property, default = nil, format = false)
     return network.search_node(plugin_provider, name, property, default, format)
   end
-  
-  #---
   
   def set_setting(property, value = nil)
     network.set_node_setting(plugin_provider, name, property, value)
     return self
   end
-  
-  #---
   
   def delete_setting(property)
     network.delete_node_setting(plugin_provider, name, property)
@@ -55,51 +47,57 @@ class Node < Base
   
   #-----------------------------------------------------------------------------
   
+  def groups
+    search(:groups, [], :array)
+  end
+  
+  #-----------------------------------------------------------------------------
+  
   def machine
-    return get(:machine, nil)
+    @machine
   end
   
   #---
   
   def machine=machine
-    set(:machine, machine)  
+    @machine = machine  
   end
   
   #---
  
   def create_machine(provider, options = {})
     if provider.is_a?(String) || provider.is_a?(Symbol)
-      set(:machine, Coral.machine(options, provider))
+      self.machine = Coral.machine(options, provider)
     end
-    return self
+    self
   end
  
   #---
   
   def public_ip # Must be set at machine level
     return machine.public_ip if machine
-    return nil
+    nil
   end
 
   #---
   
   def private_ip # Must be set at machine level
     return machine.private_ip if machine
-    return nil
+    nil
   end
 
   #---
  
   def hostname # Must be set at machine level
     return machine.hostname if machine
-    return ''
+    ''
   end
  
   #---
  
   def state # Must be set at machine level
     return machine.state if machine
-    return nil
+    nil
   end
  
   #-----------------------------------------------------------------------------
@@ -107,21 +105,21 @@ class Node < Base
     
   def start(options = {})
     return true unless machine
-    return machine.start(options)
+    machine.start(options)
   end
   
   #---
     
   def stop(options = {})
     return true unless machine && machine.running?    
-    return machine.stop(options)
+    machine.stop(options)
   end
 
   #---
     
   def reload(options = {})
     return true unless machine && machine.created?    
-    return machine.reload(options)
+    machine.reload(options)
   end
     
   #---
@@ -150,7 +148,7 @@ class Node < Base
         return machine.destroy(config)
       end
     end
-    return true    
+    true    
   end
 
   #---
@@ -159,21 +157,21 @@ class Node < Base
     return true unless machine && machine.running?
     
     config = Config.ensure(options)        
-    return machine.exec(config.import({ :commands => commands }))  
+    machine.exec(config.import({ :commands => commands }))  
   end
    
   #---
   
   def provision(options = {})
     return true unless machine && machine.running?    
-    return machine.provision(options)  
+    machine.provision(options)  
   end
  
   #---
   
   def create_image(options = {})
     return true unless machine && machine.running?    
-    return machine.create_image(options)  
+    machine.create_image(options)  
   end
  
   #-----------------------------------------------------------------------------
@@ -181,7 +179,7 @@ class Node < Base
   
   def self.build_info(type, data)  
     data = data.split(/\s*,\s*/) if data.is_a?(String)
-    return super(type, data)
+    super(type, data)
   end
   
   #---
@@ -204,7 +202,7 @@ class Node < Base
         logger.debug("Translating node options: #{options.inspect}")  
       end
     end
-    return options
+    options
   end
   
   #---
@@ -225,13 +223,13 @@ class Node < Base
       logger.debug("Project reference info: #{info.inspect}")
       return info
     end
-    return nil
+    nil
   end
   
   #---
   
   def translate_reference(reference)
-    return self.class.translate_reference(reference)
+    self.class.translate_reference(reference)
   end             
 end
 end
