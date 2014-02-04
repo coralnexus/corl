8f1b225ce1ee5b6117ff701a661361a8c5d91bd6
Adrian Webb <adrian.webb@coraltech.net>
2014-02-04 02:14:51 -0500

    Finishing images action provider that works (tested) with rackspace, aws, and google node providers.

--- lib/coral/action/images.rb
+++ lib/coral/action/images.rb
@@ -7,13 +7,30 @@ class Images < Plugin::Action
   # Spawn action interface
   
   def normalize
-    super('coral images <node_provider>')
+    super('coral images <node_provider> [ <search_term> ... ]')
+    
+    codes :node_load_failure  => 20,
+          :image_load_failure => 21
   end
 
   #-----------------------------------------------------------------------------
   # Action operations
   
   def parse(parser)
+    parser.option_bool(:match_case, false, 
+      [ '-c', '--match-case' ], 
+      'coral.core.actions.images.options.match_case'
+    )
+    parser.option_bool(:require_all, false, 
+      [ '-r', '--require-all' ], 
+      'coral.core.actions.images.options.require_all'
+    )
+    parser.arg_str(:provider, nil, 
+      'coral.core.actions.images.options.provider'
+    )
+    parser.arg_array(:search, [], 
+      'coral.core.actions.images.options.search'
+    )
   end
   
   #---
@@ -22,8 +39,18 @@ class Images < Plugin::Action
     super do |node, network, status|
       info('coral.core.actions.images.start')
       
-      
-      
+      if node = Coral.node(:test, {}, settings[:provider])
+        if images = node.images(settings[:search], settings)
+          images.each do |image|
+            ui.info(node.render_image(image), { :prefix => false })
+          end
+          ui.success("Total of #{images.length} images found") if images.length > 1
+        else
+          status = code.image_load_failure
+        end
+      else
+        status = code.node_load_failure
+      end
       status
     end
   end
