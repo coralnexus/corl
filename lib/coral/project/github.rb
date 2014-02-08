076332d6f074004b51dc3c448c0e87088e51ef28
Adrian Webb <adrian.webb@coraltech.net>
2014-02-08 16:46:03 -0500

    Adding reference initialization and completing the init_auth override in the github project provider.

--- lib/coral/project/github.rb
+++ lib/coral/project/github.rb
@@ -13,13 +13,43 @@ class Github < Git
   def normalize
     require 'octokit'
     
-    client = Octokit::Client.new :netrc => true
-    client.login
-
-    set(:url, self.class.expand_url(get(:url), get(:ssh, false))) if get(:url)
+    if reference = delete(:reference, nil)
+      self.name = reference
+    else
+      if url = get(:url, nil)
+        self.name = url
+        set(:url, self.class.expand_url(url, get(:ssh, false)))
+      end  
+    end    
     super
   end
   
+  #-----------------------------------------------------------------------------
+  # Project operations
+  
+  def init_auth
+    super do
+      key_id  = ENV['USER'] + '@' + lookup(:ipaddress)
+      ssh_key = public_key_str
+      
+      if private_key && ssh_key
+        begin
+          client = Octokit::Client.new :netrc => true
+          client.login        
+          
+          result = client.add_deploy_key(self.name, key_id, ssh_key)
+        
+        rescue Exception => error
+          logger.error(error.inspect)
+          logger.error(error.message)
+          logger.error(Util::Data.to_yaml(error.backtrace))
+
+          ui.error(error.message, { :prefix => false }) if error.message
+        end
+      end
+    end  
+  end
+     
   #-----------------------------------------------------------------------------
   # Utilities
   
