78a09280c52d4cda32b964d5524403e86317fd15
Adrian Webb <adrian.webb@coraltech.net>
2014-02-07 19:36:56 -0500

    Some modifications to test in the seed action provider.

--- lib/coral/action/seed.rb
+++ lib/coral/action/seed.rb
@@ -18,6 +18,10 @@ class Seed < Plugin::Action
   # Action operations
   
   def parse(parser)
+    parser.option_str(:home_env_var, "HOME", 
+      '--home-env ENV_VAR', 
+      'coral.core.actions.seed.options.home_env_var'
+    )
     parser.option_str(:branch, :master, 
       '--branch BRANCH', 
       'coral.core.actions.seed.options.branch'
@@ -34,32 +38,50 @@ class Seed < Plugin::Action
     super do |node, network, status|
       info('coral.core.actions.seed.start')
       
-      status = admin_exec(status) do
-        network_path = lookup(:coral_network)
+      if node && network
+        status = admin_exec(status) do
+          network_path = lookup(:coral_network)
+          keypair      = Util::SSH.generate
         
-        project = Coral.project(extended_config(:project, {
-          :directory => network_path,
-          :url       => settings[:reference],
-          :revision  => settings[:branch],
-          :pull      => true
-        }))
-        
-        if project
-          if network.load
-            success = network.add_node(node.plugin_provider, node.hostname, {
-              :public_ip  => node.public_ip,
-              :private_ip => node.private_ip,
-              :revision   => project.revision
-            })
-            status = code.node_add_failure unless success
+          dbg(keypair, 'key pair')
+          
+          results = bootstrap_node.command(:echo, { :args => '$' + settings[:home_env_var].gsub('$', '') })
+          ui.warn(results[:error], { :prefix => false }) unless results[:error].empty?
+            
+          if results[:status] == code.success
+            if ! results[:result].empty?
+              ssh_dir = File.join(results[:result], 'ssh')
+              dbg(ssh_dir, 'ssh directory')  
+            end
           else
-            status = code.network_load_failure    
-          end     
-        else
-          status = code.project_failure  
+            status = code.home_lookup_failure
+          end
+        
+          #project = Coral.project(extended_config(:project, {
+          #  :directory => network_path,
+          #  :url       => settings[:reference],
+          #  :revision  => settings[:branch],
+          #  :pull      => true
+          #}))
+        
+          #if project
+          #  if network.load
+          #    success = network.add_node(node.plugin_provider, node.hostname, {
+          #      :public_ip  => node.public_ip,
+          #      :private_ip => node.private_ip,
+          #      :revision   => project.revision
+          #    })
+          #    status = code.node_add_failure unless success
+          #  else
+          #    status = code.network_load_failure    
+          #  end     
+          #else
+          #  status = code.project_failure  
+          #end
+          status
         end
-        status
       end
+      status
     end
   end
 end
