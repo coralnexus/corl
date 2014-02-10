e0e4247756084456fd8c1fb11330b22ca3629066
Adrian Webb <adrian.webb@coraltech.net>
2014-02-09 23:46:40 -0500

    Adding a liquid container utility class for dynamic method based interfaces.

---
+++ lib/coral_core/util/liquid.rb
@@ -0,0 +1,17 @@
+
+module Coral
+module Util
+class Liquid
+
+  def initialize(&code)
+    @code = code  
+  end
+  
+  #---
+  
+  def method_missing(method, *args, &block)  
+    @code.call(method, args, block)
+  end   
+end
+end
+end
