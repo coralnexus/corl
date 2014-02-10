504349cc88277bac16530e476ef310caeb52faf0
Adrian Webb <adrian.webb@coraltech.net>
2014-02-10 05:23:34 -0500

    Adding an initial extract action provider that allows for file and structured data transmission across ssh.

---
+++ lib/coral/action/extract.rb
@@ -0,0 +1,49 @@
+
+module Coral
+module Action
+class Extract < Plugin::Action
+ 
+  #-----------------------------------------------------------------------------
+  # Action settings
+  
+  def configure
+    super do
+      codes :extract_failure
+         
+      register :path, :str, nil do |value|
+        unless File.directory?(value)
+          warn('coral.actions.extract.errors.path', { :value => value })
+          next false
+        end
+        true
+      end
+      register :encoded, :str, nil do |value|
+        @package = Util::Package.new(value)
+        if @package.data.export.empty?
+          warn('coral.actions.extract.errors.encoded', { :value => value })
+          next false  
+        end
+        true
+      end
+    end
+  end
+  
+  #---
+  
+  def arguments
+    [ :path, :encoded ]
+  end
+ 
+  #-----------------------------------------------------------------------------
+  # Action operations
+  
+  def execute
+    super do |node, network|
+      unless @package.extract(settings[:path])
+        self.status = code.extract_failure
+      end
+    end
+  end
+end
+end
+end
