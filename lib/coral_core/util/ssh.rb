047c11e5212469856bf59656d5efc464598dc9c7
Adrian Webb <adrian.webb@coraltech.net>
2014-02-11 05:13:32 -0500

    Adding exec, upload, and download implementations to the ssh utility class.

--- lib/coral_core/util/ssh.rb
+++ lib/coral_core/util/ssh.rb
@@ -1,7 +1,7 @@
 
 module Coral
 module Util
-class SSH
+class SSH < Core
 
   #-----------------------------------------------------------------------------
   # Instance generators
@@ -71,6 +71,184 @@ class SSH
       end
       false
     end
+  end
+  
+  #-----------------------------------------------------------------------------
+  # SSH Execution interface
+  
+  @@sessions = {}
+  
+  #---
+  
+  def self.session_id(public_ip, user)
+    "#{public_ip}-#{user}"  
+  end
+  
+  #---
+  
+  def self.session(public_ip, user, port = 22, private_key = nil, reset = false)
+    require 'net/ssh'
+    
+    ssh_options = {
+      :port         => port,
+      :keys         => private_key.nil? ? [] : [ private_key ],
+      :key_data     => [],
+      :keys_only    => false,
+      :auth_methods => [ 'publickey' ]
+    }
+    
+    session_id = session_id(public_ip, user)
+    
+    unless reset || @@sessions.has_key?(session_id)
+      @@sessions[session_id] = Net::SSH.start(public_ip, user, ssh_options)
+    end
+    yield(@@sessions[session_id]) if block_given?
+    @@sessions[session_id] 
+  end
+  
+  def self.init_session(public_ip, user, port = 22, private_key = nil)
+    session(public_ip, user, port, private_key, true)  
+  end
+  
+  #---
+  
+  def self.close
+    @@sessions.keys.each do |session_id|
+      session = @@sessions[session_id]
+      session.close
+      @@sessions.delete(session_id)      
+    end
+  end
+  
+  #---
+  
+  def self.exec!(public_ip, user, commands)
+    results = []
+        
+    begin
+      session(public_ip, user) do |ssh|
+        Data.array(commands).each do |command|
+          command = command.flatten.join(' ') if command.is_a?(Array)
+          command = command.to_s
+          result  = Shell::Result.new(command)
+              
+          ssh.open_channel do |ssh_channel|
+            ssh_channel.request_pty
+            ssh_channel.exec(command) do |channel, success|
+              unless success
+                raise "Could not execute command: #{command.inspect}"
+              end
+
+              channel.on_data do |ch, data|
+                result.append_output(data)
+                yield(:output, command, data) if block_given?
+              end
+
+              channel.on_extended_data do |ch, type, data|
+                next unless type == 1
+                result.append_errors(data)
+                yield(:error, command, data) if block_given?
+              end
+
+              channel.on_request('exit-status') do |ch, data|
+                result.status = data.read_long
+              end
+
+              channel.on_request('exit-signal') do |ch, data|
+                result.status = 255
+              end
+            end
+          end
+          ssh.loop              
+          results << result
+        end
+      end
+    rescue Net::SSH::HostKeyMismatch => error
+      error.remember_host!
+      sleep 0.2
+      retry
+    end
+    results  
+  end
+  
+  def self.exec(public_ip, user, commands, options = {})
+    exec!(public_ip, user, commands, options)
+  end
+  
+  #---
+  
+  def self.download!(public_ip, user, remote_path, local_path, options = {})
+    config = Config.ensure(options)
+    
+    require 'net/scp'
+    
+    # Accepted options:
+    # * :recursive - the +remote+ parameter refers to a remote directory, which
+    # should be downloaded to a new directory named +local+ on the local
+    # machine.
+    # * :preserve - the atime and mtime of the file should be preserved.
+    # * :verbose - the process should result in verbose output on the server
+    # end (useful for debugging).
+    #
+    config.init(:recursive, true)
+    config.init(:preserve, true)
+    config.init(:verbose, true)
+    
+    blocking = config.delete(:blocking, true)
+    
+    session(public_ip, user) do |ssh|
+      if blocking
+        ssh.scp.download!(remote_path, local_path, config.export) do |ch, name, received, total|
+          yield(name, received, total) if block_given?
+        end
+      else
+        ssh.scp.download(remote_path, local_path, config.export)
+      end
+    end
+  end
+  
+  def self.download(public_ip, user, remote_path, local_path, options = {})
+    download!(public_ip, user, remote_path, local_path, options)
+  end
+  
+  #---
+  
+  def self.upload!(public_ip, user, local_path, remote_path, options = {})
+    config = Config.ensure(options)
+    
+    require 'net/scp'
+    
+    # Accepted options:
+    # * :recursive - the +local+ parameter refers to a local directory, which
+    # should be uploaded to a new directory named +remote+ on the remote
+    # server.
+    # * :preserve - the atime and mtime of the file should be preserved.
+    # * :verbose - the process should result in verbose output on the server
+    # end (useful for debugging).
+    # * :chunk_size - the size of each "chunk" that should be sent. Defaults
+    # to 2048. Changing this value may improve throughput at the expense
+    # of decreasing interactivity.
+    #
+    config.init(:recursive, true)
+    config.init(:preserve, true)
+    config.init(:verbose, true)
+    config.init(:chunk_size, 2048)
+    
+    blocking = config.delete(:blocking, true)
+    
+    session(public_ip, user) do |ssh|
+      if blocking
+        ssh.scp.upload!(local_path, remote_path, config.export) do |ch, name, sent, total|
+          yield(name, sent, total) if block_given?
+        end
+      else
+        ssh.scp.upload(local_path, remote_path, config.export)
+      end
+    end
+  end
+  
+  def self.upload(public_ip, user, remote_path, local_path, options = {})
+    upload!(public_ip, user, remote_path, local_path, options)
   end
 end
 end
