a1ffa9775a7357586dcc82a94ccc1991fdeb77e9
Adrian Webb <adrian.webb@coraltech.net>
2014-02-11 22:53:44 -0500

    Changing node parameters and adding multiple execution in the bootstrap action provider.

--- lib/coral/action/bootstrap.rb
+++ lib/coral/action/bootstrap.rb
@@ -34,67 +34,98 @@ class Bootstrap < Plugin::Action
       register :bootstrap_glob, :str, '**/*.sh'
       register :bootstrap_init, :str, 'bootstrap.sh'
       
-      register :node_name, :str, nil      
-      config[:node_provider].default = nil
+      register :bootstrap_nodes, :array, nil do |values|
+        node_plugins = Plugin.loaded_plugins(:node)
+        success      = true
+        
+        values.each do |value|
+          if info = Plugin::Node.translate_reference(value)
+            if ! node_plugins.keys.include?(info[:provider].to_sym) || info[:name].empty?
+              warn('coral.actions.bootstrap.errors.bootstrap_nodes', { :value => value, :provider => info[:provider],  :name => info[:name] })
+              success = false
+            end
+          end
+        end
+        success
+      end
     end
   end
   
   #---
   
   def ignore
-    node_ignore - [ :node_provider ]
+    [ :nodes ]
   end
   
   def arguments
-    [ :node_provider, :node_name ]
+    [ :bootstrap_nodes ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
     
   def execute
-    super do |node, network|
+    settings[:nodes] = [] # Just in case it made it through (special case)
+    
+    super do |local_node, network|
       info('coral.core.actions.bootstrap.start')
       
       if network
-        if bootstrap_node = network.node(settings[:node_provider], settings[:node_name])
-          ui_group!(bootstrap_node.hostname) do
-            home_path = extension_set(:home_path, ( ENV['USER'] == 'root' ? '/root' : ENV['HOME'] ), { :node => bootstrap_node }) 
+        if network.has_nodes? && ! settings[:bootstrap_nodes].empty?
+          # Execute action on remote nodes      
+          nodes = translate_node_references(settings[:bootstrap_nodes], network)
+      
+          Coral.batch(settings[:parallel]) do |batch_op, batch|
+            if batch_op == :add
+              # Add batch operations      
+              nodes.each do |node|
+                batch.add(node.name) do
+                  ui_group!(node.hostname) do
+                    home_path = extension_set(:home_path, ( ENV['USER'] == 'root' ? '/root' : ENV['HOME'] ), { :node => node }) 
           
-            success = bootstrap_node.bootstrap(home_path, extended_config(:bootstrap, {
-              :auth_files     => settings[:auth_files],
-              :home           => settings[:home],
-              :home_env_var   => settings[:home_env_var],
-              :bootstrap_path => settings[:bootstrap_path],
-              :bootstrap_glob => settings[:bootstrap_glob],
-              :bootstrap_init => settings[:bootstrap_init]
-            })) do |op, results|
-              case op
-              when :send_config # Modify upload configurations
-                render("Starting upload of #{results[:local_path]} to #{results[:remote_path]}")  
-              when :send_progress # Report progress of uploading files
-                render("#{results[:name]}: Sent #{results[:sent]} of #{results[:total]}")  
-              when :send_process # Process final result
-                render("Successfully finished upload of #{results[:local_path]} to #{results[:remote_path]}")
-              when :exec_config # Modify bootstrap execution configurations
-                render("Starting execution of bootstrap package")  
-              when :exec_progress # Report progress of bootstrap execution
-                if results[:type] == :error
-                  alert(results[:data], { :prefix => false })
-                else
-                  render(results[:data], { :prefix => false })
-                end  
-              when :exec_process # Process final result
-                render("Successfully finished execution of bootstrap package")     
+                    success = node.bootstrap(home_path, extended_config(:bootstrap, {
+                      :auth_files     => settings[:auth_files],
+                      :home           => settings[:home],
+                      :home_env_var   => settings[:home_env_var],
+                      :bootstrap_path => settings[:bootstrap_path],
+                      :bootstrap_glob => settings[:bootstrap_glob],
+                      :bootstrap_init => settings[:bootstrap_init]
+                    })) do |bootstrap_op, results|
+                      case bootstrap_op
+                      when :send_config # Modify upload configurations
+                        render("Starting upload of #{results[:local_path]} to #{results[:remote_path]}")  
+                      when :send_progress # Report progress of uploading files
+                        render("#{results[:name]}: Sent #{results[:sent]} of #{results[:total]}")  
+                      when :send_process # Process final result
+                        render("Successfully finished upload of #{results[:local_path]} to #{results[:remote_path]}")
+                      when :exec_config # Modify bootstrap execution configurations
+                        render("Starting execution of bootstrap package")  
+                      when :exec_progress # Report progress of bootstrap execution
+                        if results[:type] == :error
+                          alert(results[:data], { :prefix => false })
+                        else
+                          render(results[:data], { :prefix => false })
+                        end  
+                      when :exec_process # Process final result
+                        render("Successfully finished execution of bootstrap package")     
+                      end
+                      results  
+                    end
+                    self.status = node.status unless success
+                    render('We are all good!') if success
+                  end  
+                end
               end
-              results  
+            else
+              # Reduce to single status
+              batch.each do |name, status_code|
+                if status_code != code.success
+                  self.status = code.batch_error
+                  break
+                end
+              end
             end
-            self.status = bootstrap_node.status unless success
-            render('We are all good!') if success
           end
-        else
-          alert("Failed to load bootstrap node: #{settings[:node_provider]} #{settings[:node_name]}")
-          self.status = code.node_load_failure    
         end
       else
         alert("Failed to load network")
