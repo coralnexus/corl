0068d31d6ebbcc2ed1cc5f6bba37594c32e3a710
Adrian Webb <adrian.webb@coraltech.net>
2014-02-18 05:06:49 -0500

    Moving most of the bootstrap action provider into the node bootstrap method and fixing architectural issues.

--- lib/coral/action/bootstrap.rb
+++ lib/coral/action/bootstrap.rb
@@ -39,7 +39,7 @@ class Bootstrap < Plugin::Action
         values.each do |value|
           if info = Plugin::Node.translate_reference(value)
             if ! node_plugins.keys.include?(info[:provider].to_sym) || info[:name].empty?
-              warn('coral.actions.bootstrap.errors.bootstrap_nodes', { :value => value, :provider => info[:provider],  :name => info[:name] })
+              warn('coral.actions.bootstrap.errors.bootstrap_nodes', { :value => value, :node_provider => info[:provider],  :name => info[:name] })
               success = false
             end
           end
@@ -63,48 +63,18 @@ class Bootstrap < Plugin::Action
   # Operations
     
   def execute
-    settings[:nodes] = [] # Just in case it made it through (special case)
-    
-    super do |local_node, network|
-      info('coral.core.actions.bootstrap.start')
-      
+    super do |local_node, network|     
       if network
         batch_success = network.batch(settings[:bootstrap_nodes], settings[:node_provider], settings[:parallel]) do |node|
-          success = node.bootstrap(network.home, extended_config(:bootstrap, {
-            :auth_files     => settings[:auth_files],
-            :home           => settings[:home],
-            :home_env_var   => settings[:home_env_var],
-            :bootstrap_path => settings[:bootstrap_path],
-            :bootstrap_glob => settings[:bootstrap_glob],
-            :bootstrap_init => settings[:bootstrap_init]
-          })) do |op, data|
-            case op
-            when :send_config # Modify upload configurations
-              render("Starting upload of #{data[:local_path]} to #{data[:remote_path]}")  
-            when :send_progress # Report progress of uploading files
-              render("#{data[:name]}: Sent #{data[:sent]} of #{data[:total]}")  
-            when :send_process # Process final result
-              render("Successfully finished upload of #{data[:local_path]} to #{data[:remote_path]}")
-            when :exec_config # Modify bootstrap execution configurations
-              render("Starting execution of bootstrap package")  
-            when :exec_progress # Report progress of bootstrap execution
-              if data[:type] == :error
-                alert(data[:data])
-              else
-                render(data[:data])
-              end  
-            when :exec_process # Process final result
-              render("Successfully finished execution of bootstrap package")     
-            end
-            data  
-          end
-          render('We are all good!') if success
-          success  
+          render_options = { :name => node.plugin_name, :hostname => node.hostname }
+          
+          info('coral.actions.bootstrap.start', render_options)
+          node.bootstrap(network.home, extended_config(:bootstrap, settings))
+          info('coral.actions.bootstrap.success', render_options)
         end
-        self.status = code.batch_error unless batch_success
+        myself.status = code.batch_error unless batch_success
       else
-        alert("Failed to load network")
-        self.status = code.network_failure
+        myself.status = code.network_failure
       end
     end
   end
