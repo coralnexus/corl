b36c3ddf2a8cfb2cdbc60091b5629edbe97b4a02
Adrian Webb <adrian.webb@coraltech.net>
2014-02-20 22:14:01 -0500

    Removing a lot of classes and dependencies that have been moved into the Nucleon project and requiring Nucleon.

--- Gemfile
+++ Gemfile
@@ -1,18 +1,13 @@
 source "http://rubygems.org"
 
-gem "log4r", "~> 1.1"
-gem "i18n", "~> 0.6"
-gem "deep_merge", "~> 1.0"
-gem "celluloid", "~> 0.15"
+gem "nucleon", "~> 0.1"
+
 gem "sshkey", "~> 1.6"
-gem "hiera", "~> 1.3"
-gem "multi_json", "~> 1.7"
-gem "grit", "~> 2.5"
-gem "octokit", "~> 2.7"
-gem "netrc", "~> 0.7"
 gem "fog", "~> 1.20"
-gem "rgen", "~> 0.6"
+
 gem "facter", "~> 1.7"
+gem "hiera", "~> 1.3"
+
 gem "puppet", "~> 3.2"
 
 group :development do
--- lib/corl.rb
+++ lib/corl.rb
@@ -1,69 +1,21 @@
 
 #*******************************************************************************
-# CORL Core Library
+# CORL (Cluster Orchestration and Research Library)
 #
-# This provides core data elements and utilities used in the CORL gems.
+# built on Nucleon (github.com/coralnexus/nucleon)
+#
+# A framework that provides a simple foundation for growing organically in 
+# the cloud.
 #
 # Author::    Adrian Webb (mailto:adrian.webb@coralnexus.com)
 # License::   GPLv3
 
 #-------------------------------------------------------------------------------
-# Global namespace
-
-module Kernel
-   
-  def dbg(data, label = '')
-    # Invocations of this function should NOT be committed to the project
-    require 'pp'
-    
-    puts '>>----------------------'
-    unless label.empty?
-      puts label
-      puts '---'
-    end
-    pp data
-    puts '<<'
-  end
-  
-  #---  
-    
-  def corl_locate(command)
-    command = command.to_s
-    exts    = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-    ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-      exts.each do |ext|
-        exe = File.join(path, "#{command}#{ext}")
-        return exe if File.executable?(exe)
-      end
-    end
-    return nil
-  end
-    
-  #---
-  
-  def corl_require(base_dir, name)
-    name = name.to_s
-    top_level_file = File.join(base_dir, "#{name}.rb")
-    
-    require top_level_file if File.exists?(top_level_file) 
-     
-    directory = File.join(base_dir, name)
-      
-    if File.directory?(directory)
-      Dir.glob(File.join(directory, '**', '*.rb')).each do |sub_file|
-        require sub_file
-      end
-    end  
-  end
-end
-
-#-------------------------------------------------------------------------------
 # Top level properties 
 
 lib_dir          = File.dirname(__FILE__)
-core_dir         = File.join(lib_dir, 'corl_core')
+core_dir         = File.join(lib_dir, 'core')
 mixin_dir        = File.join(core_dir, 'mixin')
-mixin_config_dir = File.join(mixin_dir, 'config')
 mixin_action_dir = File.join(mixin_dir, 'action')
 macro_dir        = File.join(mixin_dir, 'macro')
 util_dir         = File.join(core_dir, 'util')
@@ -73,31 +25,19 @@ plugin_dir       = File.join(core_dir, 'plugin')
 #-------------------------------------------------------------------------------
 # CORL requirements
 
-git_location = corl_locate('git')
-
 $:.unshift(lib_dir) unless $:.include?(lib_dir) || $:.include?(File.expand_path(lib_dir))
 
 #---
   
 require 'rubygems'
 
-require 'pp'
-require 'i18n'
-require 'log4r'
-require 'log4r/configurator'
-require 'base64'
+require 'nucleon'
+
+require 'tmpdir'
 require 'sshkey'
-require 'deep_merge'
+
 require 'hiera'
 require 'facter'
-require 'yaml'
-require 'multi_json'
-require 'digest/sha1'
-require 'optparse'
-require 'thread' # Eventually depreciated
-require 'celluloid'
-require 'celluloid/autostart'
-require 'tmpdir'
 
 #---
 
@@ -105,13 +45,6 @@ require 'tmpdir'
 
 I18n.enforce_available_locales = false
 I18n.load_path << File.expand_path(File.join('..', 'locales', 'en.yml'), lib_dir)
-
-#---
-
-if git_location
-  require 'grit'
-  corl_require(util_dir, :git)
-end
 
 #---
 
@@ -126,9 +59,6 @@ end
 Dir.glob(File.join(mixin_dir, '*.rb')).each do |file|
   require file
 end
-Dir.glob(File.join(mixin_config_dir, '*.rb')).each do |file|
-  require file
-end
 Dir.glob(File.join(mixin_action_dir, '*.rb')).each do |file|
   require file
 end
@@ -138,36 +68,18 @@ end
 
 #---
 
-# Include bootstrap classes
-corl_require(core_dir, :errors)
-corl_require(core_dir, :codes)
-corl_require(util_dir, :data)
-corl_require(core_dir, :config)
-corl_require(util_dir, :interface) 
-corl_require(core_dir, :core) 
-
-#---
-
-# Include core utilities
-[ :liquid, 
-  :cli, 
-  :disk, 
-  :package, 
-  :shell, 
-  :ssh 
+# Include CORL utilities
+[ :ssh 
 ].each do |name| 
-  corl_require(util_dir, name)
+  nucleon_require(util_dir, name)
 end
 
 # Include core systems
-corl_require(core_dir, :corl)
-corl_require(core_dir, :gems)
-corl_require(core_dir, :manager)
-corl_require(plugin_dir, :base)
-corl_require(core_dir, :plugin)
-corl_require(core_dir, :facade)
+nucleon_require(core_dir, :facade)
+nucleon_require(core_dir, :plugin)
 
 #-------------------------------------------------------------------------------
 # CORL initialization
 
-CORL.initialize
+Nucleon.define_namespace :CORL
+Nucleon.reload
