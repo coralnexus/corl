52c6ebe0a04fefa5360be446fc03ada32dcfd78d
Adrian Webb <adrian.webb@coraltech.net>
2014-02-21 04:31:21 -0500

    Depreciating the node action mixin in favor of the inheritance chain in the CORL action plugin class.

--- lib/core/plugin/action.rb
+++ lib/core/plugin/action.rb
@@ -1,380 +1,155 @@
 
 module CORL
 module Plugin
-class Action < Base
-  
-  include Mixin::Action::Node
+class Action < plugin_class(:action)
   
   #-----------------------------------------------------------------------------
-  # Default option interface
-  
-  class Option
-    def initialize(provider, name, type, default, locale = nil, &validator)
-      @provider  = provider
-      @name      = name
-      @type      = type
-      @default   = default
-      @locale    = locale.nil? ? "corl.actions.#{provider}.options.#{name}" : locale
-      @validator = validator if validator
+  # Property accessor / modifiers
+    
+  def configure
+    super(:corl) do
+      node_config
+      yield if block_given?
     end
+  end
+          
+  #-----------------------------------------------------------------------------
+  # Settings
+  
+  def node_config
+    node_plugins = CORL.loaded_plugins(:node)
     
-    #---
-    
-    attr_reader :provider, :name, :type
-    attr_accessor :default, :locale, :validator
-    
-    #---
-    
-    def validate(value, node, network)
+    register :parallel, :bool, true, 'corl.core.action.options.parallel'
+    register :net_provider, :str, :default, 'corl.core.action.options.net_provider' do |value|
+      value           = value.to_sym
+      network_plugins = CORL.loaded_plugins(:network)
+      
+      unless network_plugins.keys.include?(value)
+        warn('corl.core.action.errors.network_provider', { :value => value, :choices => network_plugins.keys.join(", ") })
+        next false 
+      end
+      true
+    end
+    register :node_provider, :str, :local, 'corl.core.action.options.node_provider' do |value|
+      value = value.to_sym
+      
+      unless node_plugins.keys.include?(value)
+        warn('corl.core.action.errors.node_provider', { :value => value, :choices => node_plugins.keys.join(", ") })
+        next false
+      end
+      true  
+    end
+    register :nodes, :array, [], 'corl.core.action.options.nodes' do |values|
       success = true
-      if @validator
-        success = @validator.call(value, node, network)
+      values.each do |value|
+        if info = plugin_class(:node).translate_reference(value)
+          if ! node_plugins.keys.include?(info[:provider].to_sym) || info[:name].empty?
+            warn('corl.core.action.errors.nodes', { :value => value, :provider => info[:provider],  :name => info[:name] })
+            success = false
+          end
+        end
       end
       success
     end
   end
-
-  #-----------------------------------------------------------------------------
-  # Action plugin interface
-  
-  def self.exec_safe(provider, options)
-    action_result = nil
-    
-    begin
-      logger = CORL.logger
-      
-      logger.debug("Running corl action #{provider} with #{options.inspect}")
-      action        = CORL.action(provider, options)
-      exit_status   = action.execute
-      action_result = action.result
-      
-    rescue Exception => error
-      logger.error("Corl action #{provider} experienced an error:")
-      logger.error(error.inspect)
-      logger.error(error.message)
-      logger.error(CORL::Util::Data.to_yaml(error.backtrace))
-
-      CORL.ui.error(error.message, { :prefix => false }) if error.message
-  
-      exit_status = error.status_code if error.respond_to?(:status_code)
-    end
-    
-    CORL.remove_plugin(action) if action
-
-    exit_status = CORL.code.unknown_status unless exit_status.is_a?(Integer)
-    { :status => exit_status, :result => action_result }  
-  end
-  
-  def self.exec(provider, options, quiet = true)
-    exec_safe(provider, { :settings => Config.ensure(options), :quiet => quiet })
-  end
-  
-  def self.exec_cli(provider, args, quiet = false)
-    results = exec_safe(provider, { :args => args, :quiet => quiet })
-    results[:status]
-  end
   
   #---
-  
-  def normalize
-    args = array(delete(:args, []))
-       
-    @action_interface = Util::Liquid.new do |method, method_args|
-      options = {}
-      options = method_args[0] if method_args.length > 0
-      
-      quiet   = true
-      quiet   = method_args[1] if method_args.length > 1
-      
-      myself.class.exec(method, options, quiet)
-    end
-    
-    set(:config, Config.new)
-        
-    if get(:settings, nil)
-      # Internal processing
-      configure
-      set(:processed, true)
-      set(:settings, Config.ensure(get(:settings)))
-      
-      CORL.log_level = settings[:log_level] if settings.has_key?(:log_level)
-    else
-      # External processing
-      set(:settings, Config.new)
-      configure
-      parse_base(args)
-    end   
+         
+  def node_ignore
+    [ :parallel, :net_provider, :node_provider, :nodes ]
   end
   
-  #-----------------------------------------------------------------------------
-  # Checks
-  
-  def processed?
-    get(:processed, false)
-  end
-  
-  #-----------------------------------------------------------------------------
-  # Property accessor / modifiers
-  
-  def config
-    get(:config)
-  end
-  
-  #---
-  
-  def config_subset(names)
-    Util::Data.subset(config, names)
-  end
-  
-  #---
-   
-  def settings
-    get(:settings)
-  end
-  
-  #---
-  
-  def register(name, type, default, locale = nil)
-    name = name.to_sym
-        
-    if block_given?
-      option = Option.new(plugin_provider, name, type, default, locale) do |value, success|
-        yield(value, success)
-      end
-    else
-      option = Option.new(plugin_provider, name, type, default, locale)
-    end
-    
-    config[name]   = option
-    settings[name] = option.default if settings[name].nil?
-  end
-  
-  #---
-  
-  def remove(names)
-    Util::Data.rm_keys(config, names)
-    Util::Data.rm_keys(settings, names)
-  end
-  
-  #---
-  
-  def ignore
-    []
-  end
-    
-  def options
-    config.keys - arguments - ignore
-  end
-    
-  def arguments
-    []
-  end
-  
-  #---
-    
-  def configure
-    node_config
-    yield if block_given?
-    
-    usage = "corl #{plugin_provider} "    
-    arguments.each do |arg|
-      arg_config = config[arg.to_sym]
-      
-      if arg_config.type == :array
-        usage << "<#{arg}> ..."
-      else
-        usage << "<#{arg}> "  
-      end      
-    end
-    myself.usage = usage
-    myself
-  end
-  
-  #---
-  
-  def usage=usage
-    set(:usage, usage)
-  end
-  
-  def usage
-    get(:usage, '')
-  end
-  
-  #---
-  
-  def help
-    return @parser.help if @parser
-    usage
-  end
-  
-  #---
-  
-  def result=result
-    set(:result, result)
-  end
-  
-  def result
-    get(:result, nil)
-  end
-
   #-----------------------------------------------------------------------------
   # Operations
- 
-  def parse_base(args)    
-    logger.info("Parsing action #{plugin_provider} with: #{args.inspect}")
-    
-    @parser = Util::CLI::Parser.new(args, usage) do |parser|
-      parse(parser)      
-      extension(:parse, { :parser => parser, :config => config })
-    end
-    
-    if @parser 
-      if @parser.processed
-        set(:processed, true)
-        settings.import(Util::Data.merge([ @parser.options, @parser.arguments ], true))
-        logger.debug("Parse successful: #{export.inspect}")
-        
-      elsif @parser.options[:help] && ! quiet?
-        puts I18n.t('corl.core.exec.help.usage') + ': ' + help + "\n"
-        
-      else
-        if @parser.options[:help]
-          logger.debug("Help wanted but running in silent mode")
-        else
-          logger.warn("Parse failed for unknown reasons")
-        end
-      end
-    end
-  end
-  
-  #---
-  
-  def parse(parser)
-        
-    generate = lambda do |format, name|
-      formats = [ :option, :arg ]
-      types   = [ :bool, :int, :float, :str, :array ]
-      name    = name.to_sym
-          
-      if config.export.has_key?(name) && formats.include?(format.to_sym)
-        option_config = config[name]
-        type          = option_config.type
-        default       = option_config.default
-        locale        = option_config.locale        
-        
-        if types.include?(type.to_sym)
-          value_label = "#{type.to_s.upcase}"
-      
-          if type == :bool
-            parser.send("option_#{type}", name, default, "--[no-]#{name}", locale)        
-          elsif format == :arg
-            parser.send("#{format}_#{type}", name, default, locale)  
-          else
-            if type == :array
-              parser.send("option_#{type}", name, default, "--#{name} #{value_label},...", locale)  
-            else
-              parser.send("option_#{type}", name, default, "--#{name} #{value_label}", locale)
-            end
-          end
-        end           
-      end
-    end
-     
-    #---
-    
-    options.each do |name|
-      generate.call(:option, name)  
-    end
-    
-    arguments.each do |name|
-      generate.call(:arg, name)
-    end 
-  end
-  
-  #---
   
   def validate(node, network)
-    # TODO: Add extension hooks and logging
-    
-    # Validate all of the configurations
-    success = true
-    config.export.each do |name, option|
-      success = false unless option.validate(settings[name], node, network)
-    end
-    if success
-      # Check for missing arguments (in case of internal execution mode)
-      arguments.each do |name|        
-        if settings[name.to_sym].nil?
-          warn('corl.core.exec.errors.missing_argument', { :name => name })
-          success = false
-        end
-      end
-    end
-    if ignore.include?(:nodes)
-      settings[:nodes] = []
-    end
-    success
+    super(node, network)
   end
   
   #---
    
   def execute
-    logger.info("Executing action #{plugin_provider}")
-    
-    myself.status = code.success
-    myself.result = nil
-    
-    if processed?      
+    super(true, true) do
       node_exec do |node, network|
         hook_config = { :node => node, :network => network }
         
-        begin
-          yield(node, network) if block_given? && extension_check(:exec_init, hook_config)
-          myself.status = extension_set(:exec_exit, status, hook_config)
-        ensure
-          cleanup
-        end
-      end
-    else
-      if @parser.options[:help]
-        myself.status = code.help_wanted
-      else
-        myself.status = code.action_unprocessed
+        yield(node, network) if block_given? && extension_check(:exec_init, hook_config)
+        myself.status = extension_set(:exec_exit, status, hook_config)
       end
     end
+  end
+  
+  #---
+        
+  def node_exec
+    network = init_network
     
-    myself.status = code.unknown_status unless status.is_a?(Integer)
-    
-    if processed? && status != code.success
-      logger.warn("Execution failed for #{plugin_provider} with status #{status}: #{export.inspect}")
-      alert(Codes.render_index(status))
-    end  
-    
-    status
+    #
+    # A fork in the road...
+    #
+    if network.has_nodes? && ! settings[:nodes].empty?
+      # Execute action on remote nodes 
+      success = network.batch(settings[:nodes], settings[:node_provider], settings[:parallel]) do |node|
+        exec_config = Config.new(settings)
+        exec_config.delete(:parallel)
+        exec_config.delete(:nodes)
+        exec_config.delete(:node_provider)
+              
+        result = node.action(plugin_provider, exec_config) do |op, data|
+          ui_group(node.plugin_name) do
+            case op
+            when :config # Modify seed execution configurations
+              render("Starting remote execution of #{plugin_provider} action")  
+            when :process # Process final result
+              render("Successfully finished remote execution of #{plugin_provider} action")     
+            end
+            data = execute_remote(node, network, op, data)
+          end
+          data
+        end
+        result.status == code.success 
+      end
+      myself.status = code.batch_error unless success
+    else
+      # Execute statement locally
+      node = network.local_node
+      
+      if validate(node, network)
+        yield(node, network) if block_given?
+      else
+        puts "\n" + I18n.t('corl.core.exec.help.usage') + ': ' + help + "\n" unless quiet?
+        myself.status = code.validation_failed 
+      end
+    end
   end
   
   #---
   
-  def run
-    @action_interface
+  def init_network(path = nil)
+    # Get network configuration path
+    if CORL.admin?
+      network_path = lookup(:corl_network)
+      Dir.mkdir(network_path) unless File.directory?(network_path)
+    else
+      network_path = ( path.nil? ? Dir.pwd : File.expand_path(path) )
+    end
+    
+    # Load network if it exists
+    network_config = extended_config(:network, { :directory => network_path })
+    
+    network = CORL.network(
+      CORL.sha1(network_config), 
+      network_config, 
+      settings[:net_provider]
+    )
+    network  
   end
   
   #---
   
-  def cleanup
-    logger.info("Running cleanup for action #{plugin_provider}")
-    
-    yield if block_given?
-    
-    # Nothing to do right now
-    extension(:cleanup)
-  end
-  
-  #-----------------------------------------------------------------------------
-  # Output
-  
-  def render_options
-    settings
+  def execute_remote(node, network, op, data)
+    # Implement in sub classes if needed
+    data 
   end
 end
 end
