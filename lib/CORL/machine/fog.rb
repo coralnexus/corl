04f6e9cb1d10220a3d8a032d7fce36989a437c38
Adrian Webb <adrian.webb@coraltech.net>
2014-02-27 01:30:46 -0500

    Completing implementation of the terminal method in the fog machine provider.

--- lib/CORL/machine/fog.rb
+++ lib/CORL/machine/fog.rb
@@ -217,11 +217,7 @@ class Fog < CORL.plugin_class(:machine)
   
   def terminal(user, options = {})
     super do |config|
-      dbg(public_ip, 'public ip')
-      dbg(user, 'ssh user')
-      dbg(config.export, 'ssh config')
-      #Util::SSH.terminal(public_ip, user, config.export)
-      0     
+      Util::SSH.terminal(public_ip, user, config.export)
     end
   end
   
