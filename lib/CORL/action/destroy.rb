d100db04c68464a90088d9534657784013805bbd
Adrian Webb <adrian.webb@coraltech.net>
2014-02-27 08:25:58 -0500

    Adding an initial destroy action provider.

---
+++ lib/CORL/action/destroy.rb
@@ -0,0 +1,44 @@
+
+module CORL
+module Action
+class Destroy < Plugin::CloudAction
+ 
+  #-----------------------------------------------------------------------------
+  # Settings
+  
+  def configure
+    super do
+      codes :network_failure,
+            :destroy_failure
+      
+      register :force, :bool, false
+      
+      config[:nodes].default = nil
+    end
+  end
+  
+  #---
+  
+  def arguments
+    [ :nodes ]
+  end
+
+  #-----------------------------------------------------------------------------
+  # Operations
+   
+  def execute
+    super do |node, network|
+      info('corl.actions.destroy.start')
+      
+      if network && node
+        unless node.destroy(settings)
+          myself.status = code.destroy_failure
+        end
+      else
+        myself.status = code.network_failure
+      end
+    end
+  end
+end
+end
+end
