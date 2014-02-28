213dbaca94dddcd16e04007da7a81358786eb272
Adrian Webb <adrian.webb@coraltech.net>
2014-02-28 14:06:45 -0500

    Refactoring the destroy action provider to process on the local machine and destroy remote machines due to issues the old way.

--- lib/CORL/action/destroy.rb
+++ lib/CORL/action/destroy.rb
@@ -8,30 +8,54 @@ class Destroy < Plugin::CloudAction
   
   def configure
     super do
-      codes :network_failure,
-            :destroy_failure
+      codes :network_failure
+            
+      register :destroy_nodes, :array, nil do |values|
+        if values.nil?
+          warn('corl.actions.destroy.errors.destroy_nodes_empty')
+          next false 
+        end
+        
+        node_plugins = CORL.loaded_plugins(:node)
+        success      = true
+        
+        values.each do |value|
+          if info = CORL.plugin_class(:node).translate_reference(value)
+            if ! node_plugins.keys.include?(info[:provider].to_sym) || info[:name].empty?
+              warn('corl.actions.destroy.errors.destroy_nodes', { :value => value, :node_provider => info[:provider],  :name => info[:name] })
+              success = false
+            end
+          end
+        end
+        success
+      end
     end
   end
   
   #---
   
-  def arguments
+  def ignore
     [ :nodes ]
+  end
+  
+  def arguments
+    [ :destroy_nodes ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
    
   def execute
-    super do |node, network|
+    super do |local_node, network|
       info('corl.actions.destroy.start')
       
-      if network && node
-        unless node.destroy(settings)
-          myself.status = code.destroy_failure
+      if network
+        batch_success = network.batch(settings[:destroy_nodes], settings[:node_provider], settings[:parallel]) do |node|
+          node.destroy
         end
+        myself.status = code.batch_error unless batch_success
       else
-        myself.status = code.network_failure
+        myself.status = code.network_failure  
       end
     end
   end
