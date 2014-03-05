0c3384268a874baf500eb2b5d4c7a3eea26d4d56
Adrian Webb <adrian.webb@coraltech.net>
2014-03-04 22:53:22 -0500

    Reworking the image action provider to execute locally and batch remote nodes in case the imaging process reboots the machine (aws).

--- lib/CORL/action/image.rb
+++ lib/CORL/action/image.rb
@@ -8,28 +8,51 @@ class Image < Plugin::CloudAction
   
   def configure
     super do
-      codes :network_failure,
-            :image_create_failure  
+      codes :network_failure
+      
+      register :image_nodes, :array, nil do |values|
+        if values.nil?
+          warn('corl.actions.image.errors.image_nodes_empty')
+          next false 
+        end
+        
+        node_plugins = CORL.loaded_plugins(:node)
+        success      = true
+        
+        values.each do |value|
+          if info = CORL.plugin_class(:node).translate_reference(value)
+            if ! node_plugins.keys.include?(info[:provider].to_sym) || info[:name].empty?
+              warn('corl.actions.image.errors.image_nodes', { :value => value, :node_provider => info[:provider],  :name => info[:name] })
+              success = false
+            end
+          end
+        end
+        success
+      end  
     end
   end
   
   #---
   
-  def arguments
+  def ignore
     [ :nodes ]
   end
- 
+  
+  def arguments
+    [ :image_nodes ]
+  end
+
   #-----------------------------------------------------------------------------
   # Operations
    
   def execute
-    super do |node, network|
-      info('corl.actions.image.start')
-      
-      if network && node
-        unless node.create_image
-          myself.status = code.image_create_failure
-        end  
+    super do |local_node, network|
+      if network
+        batch_success = network.batch(settings[:image_nodes], settings[:node_provider], settings[:parallel]) do |node|
+          info('corl.actions.image.start', { :provider => node.plugin_provider, :name => node.plugin_name })
+          node.create_image
+        end
+        myself.status = code.batch_error unless batch_success  
       else
         myself.status = code.network_failure
       end    
