cdb4e2942bc53701365f5f5cbc0532b7ca897137
Adrian Webb <adrian.webb@coraltech.net>
2014-03-17 19:48:19 -0400

    Adding inclusion of the registered defaults in the corl_initialize Puppet function.

--- lib/puppet/parser/functions/corl_initialize.rb
+++ lib/puppet/parser/functions/corl_initialize.rb
@@ -9,7 +9,17 @@ This function initializes the CORL plugin system through Puppet.
     EOS
 ) do |args|    
     CORL.run do
+      # Register all Nucleon plugins defined by Puppet modules.
       CORL::Util::Puppet.register_plugins({ :puppet_scope => self })
+      
+      # Make sure defaults are evaluated first!
+      if compiler.node.classes.is_a?(Hash)
+        compiler.node.classes.each do |name, parameters|
+          if name.match(/::default:?/)            
+            compiler.evaluate_classes({ name => parameters }, self, false)
+          end
+        end
+      end
     end
   end
 end
