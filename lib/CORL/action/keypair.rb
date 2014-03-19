628b0f6dc197cfb24c6255f4c7fb91570747e3c6
Adrian Webb <adrian.webb@coraltech.net>
2014-03-19 02:10:20 -0400

    Adding a keypair action provider that generates SSH keys.

---
+++ lib/CORL/action/keypair.rb
@@ -0,0 +1,55 @@
+
+module CORL
+module Action
+class Keypair < Plugin::CloudAction
+  
+  include Mixin::Action::Keypair
+ 
+  #----------------------------------------------------------------------------
+  # Settings
+  
+  def configure
+    super do
+      codes :key_failure
+      
+      register :json, :bool, true
+      keypair_config
+    end
+  end
+  
+  #---
+  
+  def ignore
+    node_ignore
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Operations
+ 
+  def execute
+    super do |node, network|
+      if keys = keypair
+        ui.info("\n", { :prefix => false })
+        ui_group(Util::Console.cyan("#{keys.type.upcase} SSH keypair")) do |ui|
+          ui.info("-----------------------------------------------------")
+        
+          if settings[:json]
+            private_key = Util::Console.blue(Util::Data.to_json(keys.encrypted_key, true))
+            ssh_key     = Util::Console.green(Util::Data.to_json(keys.ssh_key, true))  
+          else
+            private_key = Util::Console.blue(keys.encrypted_key)
+            ssh_key     = Util::Console.green(keys.ssh_key)       
+          end
+        
+          ui.info("SSH private key:\n#{private_key}")
+          ui.info("SSH public key:\n#{ssh_key}")
+          ui.info("\n", { :prefix => false }) 
+        end          
+      else
+        myself.status = code.key_failure  
+      end
+    end
+  end
+end
+end
+end
