ba05a4366baf9ecf0c24fe2bf0f18e52906af69b
Adrian Webb <adrian.webb@coraltech.net>
2014-03-20 00:06:44 -0400

    Updating the build system of the base provisioner plugin.

--- lib/core/plugin/provisioner.rb
+++ lib/core/plugin/provisioner.rb
@@ -38,7 +38,7 @@ class Provisioner < CORL.plugin_class(:base)
   end
   
   def directory
-    myself[:directory]
+    File.join(network.directory, myself[:directory])
   end
   
   #---
@@ -136,89 +136,79 @@ class Provisioner < CORL.plugin_class(:base)
   #---
   
   def build(options = {})
-    config = Config.ensure(options)
-    
-    locations    = Config.new({ :package => {} })
+    config       = Config.ensure(options)
+    success      = true    
+    locations    = Config.new({ :build => id.to_s, :package => {} })
     package_info = Config.new
     
-    init_location = lambda do |name, ext = '', base_directory = nil|
-      name           = name.to_sym      
-      base_directory = directory if base_directory.nil?
-      build_base     = build_directory
-          
-      # Create directory
-      locations[name] = File.join(locations[:build], name.to_s)        
-      FileUtils.mkdir_p(File.join(build_base, locations[name]))
-     
-      # Copy directory contents
-      unless ext.nil?
-        ext             = ! ext.to_s.empty? ? ".#{ext}" : ''
-        local_directory = File.join(base_directory, name.to_s)
-        
-        if File.directory?(local_directory)  
-          unless Dir.glob(File.join(local_directory, "*#{ext}")).empty?            
-            FileUtils.cp_r(local_directory, File.join(build_base, locations[:build]))
-          end
-        end
-        
-        # Copy gateway file
-        gateway_file = File.join(base_directory, "#{name}#{ext}")
-      
-        if File.exists?(gateway_file)
-          FileUtils.cp(gateway_file, File.join(build_base, locations[:build]))
-        end
-      end  
-    end
-    
     init_package = lambda do |name, reference|
-      package_directory = File.join(locations[:packages], id(name).to_s)
+      package_directory = File.join(locations[:build], 'packages', id(name).to_s)
+      package_success   = true
+      
+      ui.info("Building package #{blue(name)} at #{purple(reference)} into #{green(package_directory)}")
         
       project = CORL.configuration(extended_config(:package, {
         :directory => File.join(build_directory, package_directory),
         :url       => reference,
         :create    => true
       }))
-      raise unless project
+      unless project
+        ui.warn("Project #{cyan(name)} failed to initialize")
+        package_success = false
+      end
       
-      package_info.import(project.export)
-      locations[:package][name] = package_directory
+      if package_success
+        package_info.import(project.export)
+        locations[:package][name] = package_directory
       
-      if project.get([ :provisioners, plugin_provider ], false)
-        project.get_hash([ :provisioners, plugin_provider ]).each do |prov_name, info|
-          if info.has_key?(:packages)
-            info[:packages].each do |sub_name, sub_reference|
-              init_package.call(sub_name, sub_reference)
+        if project.get([ :provisioners, plugin_provider ], false)
+          project.get_hash([ :provisioners, plugin_provider ]).each do |prov_name, info|
+            if info.has_key?(:packages)
+              info[:packages].each do |sub_name, sub_reference|
+                unless init_package.call(sub_name, sub_reference)
+                  package_success = false
+                  break
+                end
+              end
             end
           end
         end
       end
+      package_success
     end
     
-    locations[:build]     = id.to_s
     local_build_directory = File.join(build_directory, locations[:build])   
     
     FileUtils.rm_rf(local_build_directory)
     FileUtils.mkdir_p(local_build_directory)
     
-    init_location.call(:packages, nil)
-    
     # Build packages
     packages.each do |name, reference|
-      init_package.call(name, reference)
+      unless init_package.call(name, reference)
+        success = false
+        break
+      end
     end
-     
-    yield(locations, package_info, init_location) if block_given?
     
-    myself[:build_locations] = locations.export
-    myself[:build_info]      = package_info.get([ :provisioners, plugin_provider ])
-    myself[:build_profiles]  = find_profiles
+    if success
+      # Build provider specific components 
+      success = yield(locations, package_info) if block_given?
     
-    network.save(config.import({
-      :commit      => true,
-      :allow_empty => true, 
-      :message     => config.get(:message, "Built #{plugin_provider} provisioner #{plugin_name}"),
-      :remote      => config.get(:remote, :edit)
-    }))
+      if success
+        # Save the updates (should build the same on all nodes in same system)
+        myself[:build_locations] = locations.export
+        myself[:build_info]      = package_info.get([ :provisioners, plugin_provider ])
+        myself[:build_profiles]  = find_profiles
+    
+        success = network.save(config.import({
+          :commit      => true,
+          :allow_empty => true, 
+          :message     => config.get(:message, "Built #{plugin_provider} provisioner #{plugin_name}"),
+          :remote      => config.get(:remote, :edit)
+        }))
+      end
+    end
+    success
   end
   
   #---
@@ -310,7 +300,7 @@ class Provisioner < CORL.plugin_class(:base)
         str.to_s.split('__')  
       end.flatten
     else
-      components = [ components.to_s ]
+      components = [ components.to_s.split('__') ].flatten
     end
     
     if capitalize
