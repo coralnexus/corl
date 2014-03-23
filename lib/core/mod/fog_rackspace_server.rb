c617465d7aa1ad7bcc30118b306e7733f337545f
Adrian Webb <adrian.webb@coraltech.net>
2014-03-22 22:24:36 -0400

    Adding a fog rackspace server model override to catch a net ssh disconnect error.

---
+++ lib/core/mod/fog_rackspace_server.rb
@@ -0,0 +1,28 @@
+module Fog
+module Compute
+class RackspaceV2
+class Server
+  
+  def setup(credentials = {})
+    requires :ssh_ip_address, :identity, :public_key, :username
+
+    commands = [
+      %{mkdir .ssh},
+      %{echo "#{public_key}" >> ~/.ssh/authorized_keys},
+      password_lock,
+      %{echo "#{Fog::JSON.encode(attributes)}" >> ~/attributes.json},
+      %{echo "#{Fog::JSON.encode(metadata)}" >> ~/metadata.json}
+    ]
+    commands.compact
+
+    @password = nil if password_lock
+    
+    Fog::SSH.new(ssh_ip_address, username, credentials).run(commands)
+  rescue Errno::ECONNREFUSED, Net::SSH::Disconnect
+    sleep(1)
+    retry
+  end  
+end
+end
+end
+end
