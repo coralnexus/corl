52615c1946283f0de4819aaa16ff51e429d5522b
Adrian Webb <adrian.webb@coraltech.net>
2014-04-13 06:57:00 -0400

    Adding a base corl vagrant action for subclassing specialized vagrant actions.

---
+++ lib/core/vagrant/action.rb
@@ -0,0 +1,39 @@
+
+module VagrantPlugins
+module CORL
+class BaseAction
+  
+  #-----------------------------------------------------------------------------
+  # Constructor / Destructor
+  
+  def initialize(app, env)
+    @app     = app
+    @env     = env[:machine].env
+     
+    @network = nil
+    @node    = nil
+    @vm      = nil
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Property accessor / modifiers
+  
+  attr_reader :network, :node, :vm
+  
+  #-----------------------------------------------------------------------------
+  # Action execution
+  
+  def call(env)
+    # Hackish solution to ensure our code has access to Vagrant machines.
+    # This serves as a Vagrant VM manager.
+    ::CORL::Vagrant.command = Command::Launcher.new([], @env)
+    
+    if @network = ::CORL::Vagrant::Config.load_network(env[:root_path])
+      @vm   = env[:machine]
+      @node = network.node(:vagrant, @vm.name) if @vm
+      yield if block_given? && @node 
+    end
+  end
+end    
+end
+end
