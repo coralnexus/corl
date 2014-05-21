4b84d0ec7d9bb1c388d810ac122583857f4c26c6
Adrian Webb <adrian.webb@coraltech.net>
2014-05-21 18:06:54 -0400

    Saving box if cached when deleting cache on Vagrant destroy.

--- lib/core/vagrant/actions/delete_cache.rb
+++ lib/core/vagrant/actions/delete_cache.rb
@@ -9,7 +9,19 @@ class DeleteCache < BaseAction
       @app.call env
       
       env[:ui].info I18n.t("corl.vagrant.actions.delete_cache.start")
+      
+      # Keep the box (in case we want to start from a saved image)
+      box     = node.cache_setting(:box)
+      box_url = node.cache_setting(:box_url)
+      
+      # Clear cache
       node.clear_cache
+      
+      if box && box_url
+        # Re-add boxes if needed
+        node.set_cache_setting(:box, box)
+        node.set_cache_setting(:box_url, box_url)  
+      end
     end
   end
 end
