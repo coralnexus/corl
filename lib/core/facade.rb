b4bf5499ca42ada5f13eca7229ab4f7d2a94400f
Adrian Webb <adrian.webb@coraltech.net>
2014-06-02 02:47:16 -0400

    Updating the corl facade to pass the namespace in plugin methods.

--- lib/core/facade.rb
+++ lib/core/facade.rb
@@ -37,42 +37,42 @@ module Facade
   # Core plugin type facade
   
   def configuration(options, provider = nil)
-    plugin(:configuration, provider, options)
+    plugin(:CORL, :configuration, provider, options)
   end
   
   def configurations(data, build_hash = false, keep_array = false)
-    plugins(:configuration, data, build_hash, keep_array)
+    plugins(:CORL, :configuration, data, build_hash, keep_array)
   end
   
   #-----------------------------------------------------------------------------
   # Cluster plugin type facade
    
   def network(name, options = {}, provider = nil)
-    plugin(:network, provider, Config.ensure(options).import({ :name => name }))
+    plugin(:CORL, :network, provider, Config.ensure(options).import({ :name => name }))
   end
   
   def networks(data, build_hash = false, keep_array = false)
-    plugins(:network, data, build_hash, keep_array)
+    plugins(:CORL, :network, data, build_hash, keep_array)
   end
    
   #---
   
   def node(name, options = {}, provider = nil)
-    plugin(:node, provider, Config.ensure(options).import({ :name => name }))
+    plugin(:CORL, :node, provider, Config.ensure(options).import({ :name => name }))
   end
   
   def nodes(data, build_hash = false, keep_array = false)
-    plugins(:node, data, build_hash, keep_array)
+    plugins(:CORL, :node, data, build_hash, keep_array)
   end
   
   #---
   
   def provisioner(options, provider = nil)
-    plugin(:provisioner, provider, options)
+    plugin(:CORL, :provisioner, provider, options)
   end
   
   def provisioners(data, build_hash = false, keep_array = false)
-    plugins(:provisioner, data, build_hash, keep_array)
+    plugins(:CORL, :provisioner, data, build_hash, keep_array)
   end
 end
 end
