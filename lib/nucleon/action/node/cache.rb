736ac73386bddf1ec86fb74cc4724016dfbcb70c
Adrian Webb <adrian.webb@coraltech.net>
2014-07-06 05:28:45 -0400

    Reworking the cache node action provider to function similar to node fact and group providers.

--- lib/nucleon/action/node/cache.rb
+++ lib/nucleon/action/node/cache.rb
@@ -4,25 +4,30 @@ module Action
 module Node
 class Cache < CORL.plugin_class(:nucleon, :cloud_action)
   
+  include Mixin::Action::Registration
+  
   #-----------------------------------------------------------------------------
   # Info
   
   def self.describe
-    super(:node, :cache, 575)
+    super(:node, :cache, 600)
   end
- 
+  
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
-      register :name, :str, ''
-      register :value, :str, ''
+      codes :cache_save_failed
       
-      register :delete, :bool, false
-      register :clear, :bool, false
+      register_str :name
+      register_str :value
       
-      register :json, :bool, false
+      register_bool :delete
+      register_bool :clear     
+      
+      register_translator :input_format            
+      register_translator :format, :json
     end
   end
   
@@ -38,36 +43,89 @@ class Cache < CORL.plugin_class(:nucleon, :cloud_action)
   def execute
     super do |node, network|
       ensure_node(node) do
-        delete_setting = settings.delete(:delete, false)
-        clear_settings = settings.delete(:clear, false)
-        use_json       = settings.delete(:json, false)
-        
-        if clear_settings
-          node.clear_cache
+        if settings.delete(:clear, false)
+          clear_node_cache(node)
+        elsif ! settings[:name] || settings[:name].empty?
+          render_node_cache(node)
         else
-          if settings[:name].empty?
-            myself.result = node.cache_setting([], {}, :hash)
-            $stderr.puts Util::Data.to_json(result, true)
-            
+          settings[:name]  = settings[:name].split('.')
+          settings[:value] = nil if settings[:value].is_a?(String) && settings[:value].empty? 
+          
+          if settings.delete(:delete, false)
+            delete_node_cache(node, settings[:name])                
+          elsif ! settings[:value].nil?
+            set_node_cache(node, settings[:name], settings[:value])
           else
-            settings[:name] = settings[:name].split('.')
-            
-            if delete_setting
-              node.delete_cache_setting(settings[:name])
-                
-            elsif ! settings[:value].empty?
-              settings[:value] = Util::Data.parse_json(settings[:value]) if use_json
-              myself.result = settings[:value]           
-              node.set_cache_setting(settings[:name], settings[:value])
-              
-            else
-              myself.result = node.cache_setting(settings[:name])
-              $stderr.puts Util::Data.to_json(result, true)
-            end
+            render_node_cache_property(node, settings[:name])
           end
-        end 
+        end
       end
     end
+  end
+  
+  #-----------------------------------------------------------------------------
+  # Sub operations
+  
+  def render_node_cache(node)
+    format        = settings[:format]
+    myself.result = node.cache_setting([], {}, :hash)
+    render result, :format => format  
+  end
+  
+  #---
+  
+  def render_node_cache_property(node, name)
+    format        = settings[:format]
+    myself.result = node.cache_setting(name)
+    render result, :format => format 
+  end
+  
+  #---
+  
+  def delete_node_cache(node, name)
+    node.delete_cache_setting(name)
+    
+    if node.cache.status == code.success
+      success("Cached property #{name.join('.')} deleted", { :i18n => false })
+    else
+      error("Cached property #{name.join('.')} deletion could not be saved", { :i18n => false })
+      myself.status = code.cache_save_failed  
+    end  
+  end
+  
+  #---
+  
+  def set_node_cache(node, name, value)
+    input_format = settings[:input_format]
+    
+    value        = Util::Data.value(render(value, { 
+      :format => input_format, 
+      :silent => true 
+    })) if input_format
+            
+    myself.result = value
+        
+    node.set_cache_setting(name, value)
+    
+    if node.cache.status == code.success  
+      success("Cached property #{name.join('.')} saved", { :i18n => false })
+    else
+      error("Cached property #{name.join('.')} could not be saved", { :i18n => false })
+      myself.status = code.cache_save_failed  
+    end  
+  end
+  
+  #---
+  
+  def clear_node_cache(node)
+    node.clear_cache
+    
+    if node.cache.status == code.success
+      success("Cached properties cleared", { :i18n => false })
+    else
+      error("Cached properties could not be cleared", { :i18n => false })
+      myself.status = code.cache_save_failed  
+    end  
   end
 end
 end
