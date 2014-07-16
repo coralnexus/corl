2c1bc52f1b39bf6d762470cee2023d673df6a36d
Adrian Webb <adrian.webb@coraltech.net>
2014-07-15 20:55:37 -0400

    Updating locale and output messages on the cloud vagrantfile action provider.

--- lib/nucleon/action/cloud/vagrantfile.rb
+++ lib/nucleon/action/cloud/vagrantfile.rb
@@ -19,7 +19,15 @@ class Vagrantfile < CORL.plugin_class(:nucleon, :cloud_action)
       codes :vagrant_backup_failure,
             :vagrant_save_failure,
             :network_save_failure
+            
+      register_bool :save
     end
+  end
+  
+  #---
+  
+  def ignore
+    node_ignore
   end
   
   #-----------------------------------------------------------------------------
@@ -27,8 +35,6 @@ class Vagrantfile < CORL.plugin_class(:nucleon, :cloud_action)
    
   def execute
     super do |node|
-      info('corl.actions.vagrantfile.start')
-      
       ensure_network do
         generated_vagrantfile_name = File.join(CORL.lib_path, 'core', 'vagrant', 'Vagrantfile')        
         project_vagrantfile_name   = File.join(network.directory, 'Vagrantfile')
@@ -36,24 +42,36 @@ class Vagrantfile < CORL.plugin_class(:nucleon, :cloud_action)
                 
         corl_vagrantfile = Util::Disk.read(generated_vagrantfile_name)
         
-        if File.exists?(project_vagrantfile_name)
-          unless FileUtils.mv(project_vagrantfile_name, "#{project_vagrantfile_name}.backup", :force => true)
-            myself.status = code.vagrant_backup_failure
-            success       = false
-          end
-        end
-        
-        if success
-          unless Util::Disk.write(project_vagrantfile_name, corl_vagrantfile)
-            myself.status = code.vagrant_save_failure
-            success       = false
-          end
-          
-          if success
-            unless network.save({ :files => 'Vagrantfile' })
-              myself.status = code.network_save_failure
+        if settings[:save]
+          if File.exists?(project_vagrantfile_name)
+            backup_file = "#{project_vagrantfile_name}.backup"
+            
+            unless FileUtils.mv(project_vagrantfile_name, backup_file, :force => true)
+              error('file_save', { :file => blue(backup_file) })
+              myself.status = code.vagrant_backup_failure
+              success       = false
             end
           end
+        
+          if success
+            unless Util::Disk.write(project_vagrantfile_name, corl_vagrantfile)
+              error('file_save', { :file => blue(project_vagrantfile_name) })
+              myself.status = code.vagrant_save_failure
+              success       = false
+            end
+          
+            if success
+              dbg(settings[:net_remote])
+              if network.save({ :files => 'Vagrantfile', :remote => settings[:net_remote], :message => "Saving new Vagrantfile.", :allow_empty => true })
+                success('update', { :file => blue('Vagrantfile'), :remote_text => yellow(remote_message(settings[:net_remote])) })
+              else
+                error('update', { :file => blue('Vagrantfile') })
+                myself.status = code.network_save_failure
+              end
+            end
+          end
+        else
+          puts corl_vagrantfile
         end 
       end
     end
