c3f87e5f4d392c25536abce799d51f9b1af62b45
Adrian Webb <adrian.webb@coraltech.net>
2014-09-30 22:19:13 -0400

    Adding some temporary debugging to test node seeding into network.

--- lib/nucleon/action/node/seed.rb
+++ lib/nucleon/action/node/seed.rb
@@ -3,17 +3,17 @@ module Nucleon
 module Action
 module Node
 class Seed < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :seed, 625)
   end
-  
+
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       codes :key_store_failure,
@@ -22,47 +22,47 @@ class Seed < Nucleon.plugin_class(:nucleon, :cloud_action)
             :node_load_failure,
             :node_save_failure
       #---
-      
+
       register_project :project_reference
-      register_str :project_branch, 'master'      
+      register_str :project_branch, 'master'
     end
   end
-  
+
   #---
-  
+
   def arguments
     [ :project_reference ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
-   
+
   def execute
     super do |node|
       info('start')
-      
+
       ensure_node(node) do
         admin_exec do
           network_path = lookup(:corl_network)
           backup_path  = File.join(Dir.tmpdir(), 'corl')
-          
+
           info('deploy_keys')
-          
+
           project_class = CORL.plugin_class(:nucleon, :project)
-          
+
           if keys = Util::SSH.generate.store
             if @project_info = project_class.translate_reference(settings[:project_reference], true)
               project_info = Config.new(@project_info)
             else
               project_info = Config.new({ :provider => :git })
             end
-            
+
             project_class.clear_provider(network_path)
-            
+
             info('backup')
             FileUtils.rm_rf(backup_path)
             FileUtils.mv(network_path, backup_path)
-            
+
             info('seeding')
             project = CORL.project(extended_config(:project, {
               :directory   => network_path,
@@ -75,28 +75,34 @@ class Seed < Nucleon.plugin_class(:nucleon, :cloud_action)
               :internal_ip => CORL.public_ip, # Needed for seeding Vagrant VMs,
               :new         => true
             }), project_info[:provider])
-            
+
             if project
               info('finalizing')
               FileUtils.chmod_R(0600, network_path)
               FileUtils.rm_rf(backup_path)
-              
+
               info('reinitializing')
               init_network
-              
+
               if network.load
                 if node = network.local_node(true)
                   info('updating')
-                  myself.status = code.node_save_failure unless node.save  
+                  # TODO: Remove (need versioned because we need to check right after bootstrap)
+                  # If local node exists and node provider is not local, remove local
+                  Nucleon.dump_enabled=true
+                  dbg(network.export, 'network')
+                  dbg(node.export, 'node')
+
+                  myself.status = code.node_save_failure unless node.save
                 else
                   myself.status = code.node_load_failure
-                end                  
+                end
               else
-                myself.status = code.network_load_failure    
-              end     
+                myself.status = code.network_load_failure
+              end
             else
-              myself.status = code.project_failure  
-            end            
+              myself.status = code.project_failure
+            end
           else
             myself.status = code.key_store_failure
           end
