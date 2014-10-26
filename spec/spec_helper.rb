f68e11cff18f4b8c791d059879c2304a7f70a1cb
Adrian Webb <adrian.webb@coraltech.net>
2014-10-26 02:15:59 -0400

    Removing old outdated rspec includes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,10 +3,7 @@ require 'rspec'
 require 'stringio'
 require 'corl'
 
-require 'corl_test_kernel'
-require 'corl_mock_input'
-
-#-------------------------------------------------------------------------------
+#*******************************************************************************
 
 RSpec.configure do |config|
   config.mock_framework = :rspec
