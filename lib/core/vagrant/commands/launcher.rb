b542acbd4831571d95a500eb3474a465f1231e06
Adrian Webb <adrian.webb@coraltech.net>
2014-11-27 16:15:33 -0500

    Changing help command label in the Vagrant corl launcher.

--- lib/core/vagrant/commands/launcher.rb
+++ lib/core/vagrant/commands/launcher.rb
@@ -3,38 +3,38 @@ module VagrantPlugins
 module CORL
 module Command
 class Launcher < ::Vagrant.plugin("2", :command)
-  
+
   include ::CORL::Parallel # Mainly for auto locking of resources
-  
+
   #-----------------------------------------------------------------------------
-  
+
   def self.synopsis
     "execute CORL actions within the defined network"
   end
- 
+
   #-----------------------------------------------------------------------------
   # Property accessors / modifiers
-  
+
   def env
     @env
   end
-  
+
   #-----------------------------------------------------------------------------
   # Execution
 
   def execute
     # Set the base command so we can access in any actions executed
     ::CORL::Vagrant.command = ::CORL.handle(self)
-    ::CORL.executable(ARGV - [ 'corl', '--' ], "[ vagrant ] corl")
+    ::CORL.executable(ARGV - [ 'corl', '--' ], "vagrant corl")
   end
-  
+
   #-----------------------------------------------------------------------------
   # Utilities
-  
+
   def vm_machine(name, provider = nil, refresh = false)
     machine = nil
-    
-    # Mostly derived from Vagrant base command with_target_vms() method 
+
+    # Mostly derived from Vagrant base command with_target_vms() method
     provider = provider.to_sym if provider
 
     env.active_machines.each do |active_name, active_provider|
@@ -54,10 +54,10 @@ class Launcher < ::Vagrant.plugin("2", :command)
     end
 
     provider ||= env.default_provider
-    
+
     machine = env.machine(name, provider, refresh)
     machine.ui.opts[:color] = :default # TODO: Something better??
-    
+
     machine
   end
 end
