1c139a1e0942ac1558bab774f17b25906c029d0a
Adrian Webb <adrian.webb@coraltech.net>
2014-12-01 22:27:26 -0500

    Not filtering for running nodes in the node destroy action provider.

--- lib/nucleon/action/node/destroy.rb
+++ lib/nucleon/action/node/destroy.rb
@@ -3,38 +3,38 @@ module Nucleon
 module Action
 module Node
 class Destroy < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :destroy, 580)
   end
- 
+
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       register_bool :force, false
-      
+
       register_nodes :destroy_nodes
     end
   end
-  
+
   #---
-  
+
   def ignore
     [ :nodes ]
   end
-  
+
   def arguments
     [ :destroy_nodes ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
-   
+
   def execute
     super do |local_node|
       ensure_network do
@@ -42,17 +42,17 @@ class Destroy < Nucleon.plugin_class(:nucleon, :cloud_action)
           answer = 'YES'
         else
           message = render_message('prompt', { :operation => :ask }) + "\n\n"
-        
+
           array(settings[:destroy_nodes]).each do |hostname|
             message << "  #{hostname}\n"
           end
-        
+
           message << "\n" + render_message('yes_query', { :operation => :ask, :yes => 'YES' }) + ' '
           answer = ask(message)
         end
-        
+
         if answer.upcase == 'YES'
-          batch_success = network.batch(settings[:destroy_nodes], settings[:node_provider], settings[:parallel]) do |node|
+          batch_success = network.batch(settings[:destroy_nodes], settings[:node_provider], settings[:parallel], false) do |node|
             info('start', { :provider => node.plugin_provider, :name => node.plugin_name })
             node.destroy
           end
