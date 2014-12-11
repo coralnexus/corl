e2df752535d4d6170f995a5fc11d3836bf085f71
Adrian Webb <adrian.webb@coraltech.net>
2014-12-11 17:32:56 -0500

    Removing the node ensure in the node exec action provider.

--- lib/nucleon/action/node/exec.rb
+++ lib/nucleon/action/node/exec.rb
@@ -3,48 +3,46 @@ module Nucleon
 module Action
 module Node
 class Exec < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :exec, 605)
   end
- 
+
   #----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       register :command, :array, nil
     end
   end
-  
+
   #---
-  
+
   def arguments
     [ :command ]
   end
- 
+
   #-----------------------------------------------------------------------------
   # Operations
-   
+
   def execute
     super do |node|
-      ensure_node(node) do
-        if settings[:command].length > 1
-          settings[:command].collect! do |value|
-            if value.strip.match(/\s+/)
-              value = "\"#{value}\""
-            end
-            value
+      if settings[:command].length > 1
+        settings[:command].collect! do |value|
+          if value.strip.match(/\s+/)
+            value = "\"#{value}\""
           end
+          value
         end
-        
-        command_str   = settings[:command].join(' ')
-        result        = node.exec({ :commands => [ command_str ] }).first
-        myself.status = result.status
       end
+
+      command_str   = settings[:command].join(' ')
+      result        = node.exec({ :commands => [ command_str ] }).first
+      myself.status = result.status
     end
   end
 end
