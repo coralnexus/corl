0fd962eb0434b1856103b81e2f1bf140033d4d83
Adrian Webb <adrian.webb@coraltech.net>
2014-12-19 14:16:04 -0500

    Removing the parallel inclusion in the build class.

--- lib/core/build.rb
+++ lib/core/build.rb
@@ -1,37 +1,35 @@
 
 module CORL
 class Build < Core
-  
-  include Parallel
-  
+
   #-----------------------------------------------------------------------------
   # Constructor / destructor
-  
+
   def initialize
     @config    = Config.new
     @locations = Config.new
-    
+
     @plurals   = {}
     @types     = {}
   end
-  
+
   #-----------------------------------------------------------------------------
   # Build lock
-  
+
   @@build_lock = Mutex.new
- 
+
   #---
-  
+
   def build_lock
     @@build_lock
   end
-  
+
   #-----------------------------------------------------------------------------
   # Type registration
-  
+
   def register(type, plural = nil)
     type = type.to_sym
-    
+
     if plural
       plural = plural.to_sym
     else
@@ -40,73 +38,73 @@ class Build < Core
     @plurals[plural] = type
     @types[type]     = Config.new
   end
-    
+
   #-----------------------------------------------------------------------------
   # Package configuration
-  
+
   def config
-    @config  
+    @config
   end
-  
+
   def import(config)
-    @config.import(config)  
+    @config.import(config)
   end
- 
+
   #-----------------------------------------------------------------------------
   # Build locations
-  
+
   def locations
     @locations
   end
-  
+
   def set_location(provider, name, directory)
     @locations.set([ provider, name ], directory)
   end
-  
+
   def remove_location(provider, name = nil)
     @locations.delete([ provider, name ])
   end
-  
+
   #-----------------------------------------------------------------------------
   # Addon build types
-    
+
   def method_missing(method, *args, &code)
     success = false
     result  = nil
-      
+
     if method.to_s.match(/^set\_([a-z].*)$/)
       name = $1.to_sym
-      
+
       if @types.has_key?(name) && args.length > 2
         @types[name].set([ args[0], args[1] ], args[2]) if args.length > 2
         success = true
       end
-      
+
     elsif method.to_s.match(/^remove\_([a-z].*)$/)
       name = $1.to_sym
-      
+
       if @types.has_key?(name) && args.length > 0
         @types[name].delete([ args[0], args[1] ])
         success = true
       end
-      
+
     else
       name = @plurals[method.to_sym]
-      
+
       if name && @types.has_key?(name)
         result  = @types[name]
         success = true
-      end  
+      end
     end
     super unless success # Raise NoMethodError
     result
   end
-  
+
   #-----------------------------------------------------------------------------
   # Builders
-  
+
   def manage(plugin_type, options = {})
     CORL.send(plugin_type, options)
-  end 
+  end
 end
 end
