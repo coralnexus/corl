ac49a1970d8b85fc63b8813d7e1eb663f168be83
Adrian Webb <adrian.webb@coraltech.net>
2014-12-25 01:53:48 -0500

    Extra whitespace removal from the node provision action provider.

--- lib/nucleon/action/node/provision.rb
+++ lib/nucleon/action/node/provision.rb
@@ -3,60 +3,60 @@ module Nucleon
 module Action
 module Node
 class Provision < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :provision, 615)
   end
- 
+
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       codes :provision_failure
-            
+
       register :dry_run, :bool, false
       register :environment, :str, ''
     end
   end
-  
+
   #---
-  
+
   def arguments
     [ :environment ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
-  
+
   def execute
     super do |node|
       info('corl.actions.provision.start')
-      
-      ensure_node(node) do        
+
+      ensure_node(node) do
         success = true
-        
+
         settings.delete(:environment) if settings[:environment] == ''
-        
+
         if settings.has_key?(:environment)
           node.create_fact(:corl_environment, settings[:environment])
-        end 
-        
+        end
+
         if CORL.admin?
           unless node.build_time && File.directory?(network.build_directory)
             success = node.build(settings)
           end
-          
+
           if success
-            provisioner_info = node.provisioner_info   
-            
+            provisioner_info = node.provisioner_info
+
             node.provisioners.each do |provider, collection|
               provider_info = provisioner_info[provider]
               profiles      = provider_info[:profiles]
-              
+
               collection.each do |name, provisioner|
                 if supported_profiles = provisioner.supported_profiles(node, profiles)
                   profile_success = provisioner.provision(node, supported_profiles, settings)
