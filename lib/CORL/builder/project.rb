6045c4cc59b4239c35ca3c44a0240b3964a5331d
Adrian Webb <adrian.webb@coraltech.net>
2014-12-25 08:05:59 -0500

    Adding an initial general project builder provider.

---
+++ lib/CORL/builder/project.rb
@@ -0,0 +1,32 @@
+
+module CORL
+module Builder
+class Project < Nucleon.plugin_class(:CORL, :builder)
+
+  #-----------------------------------------------------------------------------
+  # Project interface operations
+
+  def build_provider(provider_path, project_reference, environment)
+    path    = provider_path.to_s
+    success = true
+
+    info("Building project #{purple(project_reference)} into #{green(path)}", { :i18n => false })
+
+    full_directory = File.join(network.directory, path)
+    project        = build_config.manage(:project, extended_config(:project, {
+      :directory     => full_directory,
+      :url           => project_reference,
+      :create        => File.directory?(full_directory) ? false : true,
+      :manage_ignore => false,
+      :corl_file     => false
+    }))
+    unless project
+      warn("Project #{cyan(path)} failed to initialize", { :i18n => false })
+      success = false
+    end
+    success("Build of project #{blue(path)} finished", { :i18n => false }) if success
+    success
+  end
+end
+end
+end
