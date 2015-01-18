d8c092e76dbe3eb0f221e364c07d1c72e5adba8b
Adrian Webb <adrian.webb@coraltech.net>
2015-01-18 17:01:11 -0500

    Fixing incorrect authorized_keys generation in the revoke node action provider.

--- lib/nucleon/action/node/revoke.rb
+++ lib/nucleon/action/node/revoke.rb
@@ -3,51 +3,51 @@ module Nucleon
 module Action
 module Node
 class Revoke < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :revoke, 550)
   end
- 
+
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       codes :key_remove_failure
-      
+
       register :public_key, :str, nil
     end
   end
-  
+
   #---
-  
+
   def arguments
     [ :public_key ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
-   
+
   def execute
     super do |node|
       info('corl.actions.revoke.start')
-      
+
       ensure_node(node) do
         ssh_path        = Util::SSH.key_path
-        authorized_keys = File.join(ssh_path, 'authorized_keys')        
+        authorized_keys = File.join(ssh_path, 'authorized_keys')
         public_key      = settings[:public_key].strip
         output_keys     = []
-                
+
         if File.exists?(authorized_keys)
           Util::Disk.read(authorized_keys).split("\n").each do |line|
             if line.strip.include?(public_key)
               key_found = true
             else
-              output_keys << public_key
-            end            
+              output_keys << line
+            end
           end
         end
         unless key_found
