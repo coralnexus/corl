77f2f098d290973002cb49e39b7d4951763736f3
Adrian Webb <adrian.webb@coraltech.net>
2015-01-19 17:47:21 -0500

    Adding a debug option to the node lookup action provider.

--- lib/nucleon/action/node/lookup.rb
+++ lib/nucleon/action/node/lookup.rb
@@ -3,17 +3,17 @@ module Nucleon
 module Action
 module Node
 class Lookup < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :lookup, 565)
   end
 
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       register :properties, :array, []
@@ -26,37 +26,38 @@ class Lookup < Nucleon.plugin_class(:nucleon, :cloud_action)
         end
         success
       end
-      
+
       register_translator :format, :json
+      register_bool :debug, false
     end
   end
-  
+
   #---
-  
+
   def arguments
     [ :properties ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
-   
+
   def execute
     super do |node|
       ensure_node(node) do
         translator = CORL.translator({}, settings[:format])
-        
+
         if settings[:properties].empty?
           myself.result = node.hiera_configuration(node.facts)
           $stderr.puts translator.generate(result)
         else
           properties = {}
-          
+
           settings.delete(:properties).each do |property|
             properties[property] = node.lookup(property, nil, settings)
           end
           $stderr.puts translator.generate(properties)
-          myself.result = properties 
-        end        
+          myself.result = properties
+        end
       end
     end
   end
