1d5833d4e3b11fe82daf385ba4783cad150b4296
Adrian Webb <adrian.webb@coraltech.net>
2015-02-09 11:23:27 -0500

    Fixing lookup issue with the Puppet corl_include function.

--- lib/puppet/parser/functions/corl_include.rb
+++ lib/puppet/parser/functions/corl_include.rb
@@ -18,7 +18,7 @@ This function performs a lookup for a variable value in various locations follow
 If no value is found in the defined sources, it does not include any classes.
     EOS
 ) do |args|
-    
+
     CORL.run do
       raise(Puppet::ParseError, "corl_include(): Define at least the variable name " +
         "given (#{args.size} for 1)") if args.size < 1
@@ -26,11 +26,13 @@ If no value is found in the defined sources, it does not include any classes.
       var_name    = args[0]
       parameters  = ( args.size > 1 ? args[1] : {} )
       options     = ( args.size > 2 ? args[2] : {} )
-      
+
       module_name = parent_module_name
       contexts    = [ :include, var_name ]
-      
+
       default_options = {
+        :node            => CORL::Provisioner::Puppetnode.node,
+        :provisioner     => :puppetnode,
         :hiera_scope     => self,
         :puppet_scope    => self,
         :search          => 'core::default',
@@ -38,13 +40,13 @@ If no value is found in the defined sources, it does not include any classes.
         :merge           => true,
         :undefined_value => :undef
       }
-      
+
       if module_name
-        config = CORL::Config.init(options, contexts, module_name, default_options)  
+        config = CORL::Config.init(options, contexts, module_name, default_options)
       else
         config = CORL::Config.init_flat(options, contexts, default_options)
       end
-      
+
       CORL::Util::Puppet.include(var_name, parameters, config)
     end
   end
