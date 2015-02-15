e81372aef918abc59a668b91f5ee29b1a672b64a
Adrian Webb <adrian.webb@coraltech.net>
2015-02-15 01:51:16 -0500

    Changing the executable_init hook to executable_load in the corl_executable extension provider.

--- lib/nucleon/extension/corl_executable.rb
+++ lib/nucleon/extension/corl_executable.rb
@@ -3,7 +3,7 @@ module Nucleon
 module Extension
 class CorlExecutable < Nucleon.plugin_class(:nucleon, :extension)
 
-  def executable_init(config)
+  def executable_load(config)
     network_path = Nucleon.fact(:corl_network)
     Nucleon.load_plugins(network_path) if Dir.pwd != network_path
   end
