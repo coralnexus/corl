7926efa2ad7f86b1d2229a9fa737d68d7275e8d8
Adrian Webb <adrian.webb@coraltech.net>
2015-02-18 14:24:43 -0500

    Removing extra whitespace from the network settings macro mixin.

--- lib/core/mixin/macro/network_settings.rb
+++ lib/core/mixin/macro/network_settings.rb
@@ -3,55 +3,55 @@ module CORL
 module Mixin
 module Macro
 module NetworkSettings
-  
+
   def network_settings(_type)
-    
+
     # Networks are inherited unless explicitely set
-     
+
     define_method :network do
       plugin_parent
     end
- 
+
     define_method :network= do |network|
       myself.plugin_parent = network
     end
-  
+
     #---
-  
+
     define_method :setting do |property, default = nil, format = false|
       network.send("#{_type}_setting", plugin_provider, plugin_name, property, default, format)
     end
-  
+
     define_method :search do |property, default = nil, format = false, extra_groups = []|
       network.send("search_#{_type}", plugin_provider, plugin_name, property, default, format, extra_groups)
     end
-  
+
     define_method :set_setting do |property, value = nil|
       network.send("set_#{_type}_setting", plugin_provider, plugin_name, property, value)
     end
-  
+
     define_method :delete_setting do |property|
       network.send("delete_#{_type}_setting", plugin_provider, plugin_name, property)
     end
-  
+
     #---
- 
+
     define_method :[] do |name, default = nil, format = false|
       search(name, default, format)
     end
-  
+
     #---
-  
+
     define_method :[]= do |name, value|
       set_setting(name, value)
     end
-    
+
     #---
-  
+
     define_method :cache do
       network.cache
     end
-  
+
     define_method :cache_setting do |keys, default = nil, format = false|
       cache.get([ _type, plugin_provider, plugin_name, keys ].flatten, default, format)
     end
@@ -69,22 +69,22 @@ module NetworkSettings
     end
 
     #---
-  
+
     define_method :groups do
       array(myself[:settings])
     end
-    
+
     #---
-    
+
     define_method :add_groups do |groups|
       myself[:settings] = array(setting(:settings)) | array(groups)
     end
-    
+
     #---
-    
+
     define_method :remove_groups do |groups|
       myself[:settings] = array(setting(:settings)) - array(groups)
-    end    
+    end
   end
 end
 end
