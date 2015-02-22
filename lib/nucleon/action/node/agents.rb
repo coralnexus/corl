c90989287b5ac32b23a8a69179a5ad203823e27d
Adrian Webb <adrian.webb@coraltech.net>
2015-02-21 22:08:05 -0500

    Adding a agents node action provider that lists active / inactive agents on nodes.

---
+++ lib/nucleon/action/node/agents.rb
@@ -0,0 +1,44 @@
+
+module Nucleon
+module Action
+module Node
+class Agents < Nucleon.plugin_class(:nucleon, :cloud_action)
+
+  #-----------------------------------------------------------------------------
+  # Info
+
+  def self.describe
+    super(:node, :agents, 800)
+  end
+
+  #-----------------------------------------------------------------------------
+  # Settings
+
+  def configure
+    super do
+      register_translator :format, :json
+    end
+  end
+
+  #-----------------------------------------------------------------------------
+  # Operations
+
+  def execute
+    super do |node|
+      ensure_node(node) do
+        translator    = CORL.translator({}, settings[:format])
+        agent_records = node.agents
+
+        agent_records.each do |provider, agent_options|
+          agent_records[provider][:running] = node.agent_running(provider)
+        end
+
+        myself.result = agent_records
+        $stderr.puts translator.generate(result)
+      end
+    end
+  end
+end
+end
+end
+end
