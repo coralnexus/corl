3661e546273fcf2bc242a8613fedef3366d20b7b
Adrian Webb <adrian.webb@coraltech.net>
2015-04-25 22:46:07 -0400

    Adding a ssh_user argument to the node authorize action provider.

--- lib/nucleon/action/node/authorize.rb
+++ lib/nucleon/action/node/authorize.rb
@@ -3,53 +3,54 @@ module Nucleon
 module Action
 module Node
 class Authorize < Nucleon.plugin_class(:nucleon, :cloud_action)
-  
+
   #-----------------------------------------------------------------------------
   # Info
-  
+
   def self.describe
     super(:node, :authorize, 555)
   end
- 
+
   #-----------------------------------------------------------------------------
   # Settings
-  
+
   def configure
     super do
       codes :key_store_failure
-      
-      register :reset, :bool, false
-      register :public_key, :str, nil
+
+      register_bool :reset, false
+      register_str :public_key, nil
+      register_str :ssh_user, nil
     end
   end
-  
+
   #---
-  
+
   def arguments
-    [ :public_key ]
+    [ :public_key, :ssh_user ]
   end
 
   #-----------------------------------------------------------------------------
   # Operations
-   
+
   def execute
     super do |node|
-      info('corl.actions.authorize.start')
-      
+      info('start', { :public_key => settings[:public_key] })
+
       ensure_node(node) do
-        ssh_path        = Util::SSH.key_path
-        authorized_keys = File.join(ssh_path, 'authorized_keys')        
+        ssh_path        = Util::SSH.key_path(settings[:ssh_user])
+        authorized_keys = File.join(ssh_path, 'authorized_keys')
         public_key      = settings[:public_key].strip
         key_found       = false
-        
+
         File.delete(authorized_keys) if settings[:reset]
-                
+
         if File.exists?(authorized_keys)
           Util::Disk.read(authorized_keys).split("\n").each do |line|
             if line.strip.include?(public_key)
               key_found = true
-              break  
-            end            
+              break
+            end
           end
         end
         unless key_found
