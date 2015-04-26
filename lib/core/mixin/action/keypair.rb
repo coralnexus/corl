9fed1dad56b2c79b223e7b7e10f5ca88e407b783
Adrian Webb <adrian.webb@coraltech.net>
2015-04-25 22:44:11 -0400

    Adding ability to suppress warnings in the keypair action mixin keypair method.

--- lib/core/mixin/action/keypair.rb
+++ lib/core/mixin/action/keypair.rb
@@ -3,10 +3,10 @@ module CORL
 module Mixin
 module Action
 module Keypair
-        
+
   #-----------------------------------------------------------------------------
   # Options
-        
+
   def keypair_config
     register_str :private_key, nil, 'corl.core.mixin.action.keypair.options.private_key' do |value|
       success = true
@@ -19,53 +19,53 @@ module Keypair
           end
         else
           warn('corl.core.mixin.action.keypair.errors.private_key_not_found', { :value => file })
-          success = false  
+          success = false
         end
       end
-      success  
+      success
     end
-    
+
     register_bool :require_password, false, 'corl.core.mixin.action.keypair.options.require_password'
-    
+
     register_str :key_type, 'RSA', 'corl.core.mixin.action.keypair.options.key_type' do |value|
       key_type_choices = [ 'RSA', 'DSA' ]
       unless key_type_choices.include?(value.to_s.upcase)
         warn('corl.core.mixin.action.keypair.errors.key_type', { :value => value, :choices => key_type_choices })
-        next false    
+        next false
       end
       true
     end
     register_int :key_bits, 2048, 'corl.core.mixin.action.keypair.options.key_bits' do |value|
       unless value >= 2048
         warn('corl.core.mixin.action.keypair.errors.key_bits', { :value => value, :required => 2048 })
-        next false 
+        next false
       end
-      true     
+      true
     end
     register_str :key_comment, '', 'corl.core.mixin.action.keypair.options.key_comment'
-    
+
     config_subset(keypair_ignore)
   end
-  
+
   #---
-         
+
   def keypair_ignore
     [ :require_password, :key_type, :key_bits, :key_comment ]
   end
-  
+
   #---
-  
+
   def keypair_clean
-    remove(keypair_ignore)  
+    remove(keypair_ignore)
   end
-         
+
   #-----------------------------------------------------------------------------
   # Properties
-  
+
   @keypair = nil
-  
+
   #---
-  
+
   def keypair=options
     config = Config.ensure(options).defaults({
       :type    => settings[:key_type].to_s.upcase,
@@ -75,13 +75,13 @@ module Keypair
     @keypair = Util::SSH.generate(config)
     settings.import({ :keypair => @keypair })
   end
-  
+
   #---
-  
-  def keypair(reset = false)
+
+  def keypair(reset = false, warn = true)
     if reset || ! @keypair
       if settings[:private_key]
-        key_options = { :private_key => private_key } 
+        key_options = { :private_key => private_key }
       else
         key_options = {}
         if settings[:require_password]
@@ -89,7 +89,7 @@ module Keypair
           if key_password
             key_options[:passphrase] = key_password
           else
-            warn('no_password')
+            warn('no_password') if warn
             return nil
           end
         end
